102fcb48eb613235a602b123a6569a4c16aff864
PerchunPak <git@perchun.it>
2024-02-29 15:01:45 +0100

    Remove Gnome applications

--- nixos/gnome.nix
+++ nixos/gnome.nix
@@ -1,31 +1,27 @@
-{pkgs, lib, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   config = {
     services.xserver.enable = lib.mkDefault true;
     services.xserver.displayManager.gdm.enable = true;
     services.xserver.desktopManager.gnome.enable = true;
-    environment.gnome.excludePackages =
-      (with pkgs; [
-        gnome-photos
-        gnome-tour
-      ])
-      ++ (with pkgs.gnome; [
-        cheese # webcam tool
-        gnome-music
-        gedit # text editor
-        epiphany # web browser
-        geary # email reader
-        gnome-characters
-        tali # poker game
-        iagno # go game
-        hitori # sudoku game
-        atomix # puzzle game
-        yelp # Help view
-        gnome-contacts
-        gnome-initial-setup
-      ]);
-    programs.dconf.enable = true;
+
+    services.gnome.core-utilities.enable = false;
     environment.systemPackages = with pkgs; [
+      gnome-text-editor
+      gnome.gnome-calculator
+      gnome.gnome-logs
+      loupe # image viewer
+      gnome.nautilus # file manager
+      snapshot # screenshot utility
+      gnome.totem # video player
+      gnome.gnome-software # gui package installer
+
       gnome.gnome-tweaks
     ];
+
+    programs.dconf.enable = true;
   };
 }
