da2ba569b970086443f99d7ebfd37c80585a6239
PerchunPak <git@perchun.it>
2024-03-10 19:19:11 +0100

    Add fish aliases

--- home-manager/fish.nix
+++ home-manager/fish.nix
@@ -5,6 +5,14 @@
       set fish_greeting # Disable greeting
       tide configure --auto --style=Lean --prompt_colors="True color" --show_time=No --lean_prompt_height="One line" --prompt_spacing=Compact --icons="Few icons" --transient=No
     '';
+    shellAliases = {
+      ls = "${pkgs.eza}/bin/eza --icons";
+      la = "${pkgs.eza}/bin/eza --icons -la";
+      lt = "${pkgs.eza}/bin/eza --icons --tree";
+      docker-start = "sudo systemctl start docker";
+      kssh = "kitten ssh";
+      cop = "gh copilot suggest -t shell";
+    };
     plugins = [
       {
         name = "done";
