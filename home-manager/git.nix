e8417940af8ce1673ada2fce34480011d2bb4c87
PerchunPak <git@perchun.it>
2024-03-12 18:05:17 +0100

    Activate signing commits in git

--- home-manager/git.nix
+++ home-manager/git.nix
@@ -3,15 +3,15 @@
     enable = true;
     userName = "PerchunPak";
     userEmail = "git@perchun.it";
+    signing = {
+      key = "313F67D1EAB770F9";
+      signByDefault = true;
+    };
     extraConfig = {
       init.defaultBranch = "main";
       credential.helper = "store";
       push.autoSetupRemote = true;
       pull.rebase = true;
-      signing = {
-        key = "313F67D1EAB770F9";
-        signByDefault = true;
-      };
       url = {
         "ssh://git@github.com/" = {insteadOf = ["https://github.com/"];};
       };
