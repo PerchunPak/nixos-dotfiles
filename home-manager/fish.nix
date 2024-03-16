272ca653446272a14f80b881565e58fd0f02f178
PerchunPak <git@perchun.it>
2024-03-16 22:10:22 +0100

    Make nvim default editor

--- home-manager/fish.nix
+++ home-manager/fish.nix
@@ -1,6 +1,9 @@
 {pkgs, ...}: {
   programs.fish = {
     enable = true;
+    loginShellInit = ''
+      set -gx EDITOR nvim
+    '';
     interactiveShellInit = ''
       set fish_greeting # Disable greeting
       tide configure --auto --style=Lean --prompt_colors="True color" --show_time=No --lean_prompt_height="One line" --prompt_spacing=Compact --icons="Few icons" --transient=No
