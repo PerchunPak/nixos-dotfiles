c3283a527db950d1f30ca582b579b605ff0ea995
PerchunPak <git@perchun.it>
2024-03-18 23:22:55 +0100

    Remove resolved TODO comments

--- home-manager/gaming.nix
+++ home-manager/gaming.nix
@@ -3,7 +3,7 @@
     # TODO steam
     #protonup-qt # can I manage proton-GE from home-manager/nix?
     #protontricks
-    prismlauncher # TODO impermanence
-    xonotic # TODO impermanence?
+    prismlauncher
+    xonotic
   ];
 }
