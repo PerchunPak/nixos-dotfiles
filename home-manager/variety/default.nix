fa3a6732816133ef28b8449f911ff09ee0fe52ec
PerchunPak <git@perchun.it>
2024-03-20 17:16:56 +0100

    Fix variety

--- home-manager/variety/default.nix
+++ home-manager/variety/default.nix
@@ -1,11 +1,11 @@
 {pkgs, ...}: {
   home.packages = [pkgs.variety];
 
-  home.file.".config/autostart/variety.desktop".source = "${pkgs.variety}/share/variety/variety-autostart.desktop.template";
+  home.file.".config/autostart/variety.desktop".source = "${pkgs.variety}/share/applications/variety.desktop";
 
   xdg.configFile = {
-    #"variety/.firstrun".text = "2023-10-27 11:06:59";
-    #"variety/Downloaded/.variety_download_folder".text = "2023-10-27 11:06:59";
+    "variety/.firstrun".text = "2023-10-27 11:06:59";
+    "variety/Downloaded/.variety_download_folder".text = "2023-10-27 11:06:59";
     "variety/ui.conf".source = ./ui.conf;
     "variety/variety.conf".source = ./variety.conf;
   };
