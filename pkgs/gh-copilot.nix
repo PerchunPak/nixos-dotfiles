94c4d22af687baa6d2f15e2e7d63cee616936d17
PerchunPak <git@perchun.it>
2024-03-23 15:50:09 +0100

    Update gh-copilot

--- pkgs/gh-copilot.nix
+++ pkgs/gh-copilot.nix
@@ -9,47 +9,51 @@
   systemToPlatform = {
     "x86_64-linux" = {
       name = "linux-amd64";
-      hash = "sha256-FKzvERcVYkyy1aNYHZIftC2WvSHRxFqSG/g7gpTTvoo=";
+      hash = "sha256-uEG9wvoUyX54rcsZI2dgSfEy9d/FMfjf4+kn5wJoojY=";
     };
     "aarch64-linux" = {
       name = "linux-arm64";
-      hash = "sha256-4vX9On0upgfjM/IL/UzQj5ioeVnSsd2rUgIz6w4szZM=";
+      hash = "sha256-r0Vo9lZygIEQeSqPv1ix/NK347wqoCkaIL635qeP5ok=";
     };
     "x86_64-darwin" = {
       name = "darwin-amd64";
-      hash = "sha256-W4ElKXsMo47dVRNJEnLzH2rpvkua56lj/NkJd3R8CCE=";
+      hash = "sha256-Hu7A/M5JvwFaA5AmO1WO65D7KD3dYTGnNb0A5CqAPH0=";
     };
     "aarch64-darwin" = {
       name = "darwin-arm64";
-      hash = "sha256-F2OA66h/ptkjLZ2oQgkbZlDo31YDZzhk5Pre36TkHvI=";
+      hash = "sha256-d6db1YOmo7If/2PTkgScsTaMqZZNZl6OL/qpgYfCa3s=";
     };
   };
   platform = systemToPlatform.${system} or throwSystem;
 in
-  stdenv.mkDerivation rec {
+  stdenv.mkDerivation (finalAttrs: {
     pname = "gh-copilot";
-    version = "0.5.4-beta";
+    version = "1.0.1";
 
     src = fetchurl {
       name = "gh-copilot";
-      url = "https://github.com/github/gh-copilot/releases/download/v${version}/${platform.name}";
+      url = "https://github.com/github/gh-copilot/releases/download/v${finalAttrs.version}/${platform.name}";
       hash = platform.hash;
     };
 
     dontUnpack = true;
 
     installPhase = ''
+      runHook preInstall
+
       install -m755 -D $src $out/bin/gh-copilot
+
+      runHook postInstall
     '';
 
-    meta = with lib; {
-      changelog = "https://github.com/github/gh-copilot/releases/tag/v${version}";
+    meta = {
+      changelog = "https://github.com/github/gh-copilot/releases/tag/v${finalAttrs.version}";
       description = "Ask for assistance right in your terminal.";
       homepage = "https://github.com/github/gh-copilot";
-      sourceProvenance = with sourceTypes; [binaryNativeCode];
-      license = licenses.unfree;
-      maintainers = with maintainers; [perchun];
-      platforms = attrNames systemToPlatform;
+      license = lib.licenses.unfree;
       mainProgram = "gh-copilot";
+      maintainers = with lib.maintainers; [perchun];
+      platforms = lib.attrNames systemToPlatform;
+      sourceProvenance = with lib.sourceTypes; [binaryNativeCode];
     };
-  }
+  })
