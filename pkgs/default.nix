383bdc6f3b966e52caa1578d9a96d0a4ed0a9911
PerchunPak <git@perchun.it>
2024-03-27 17:07:36 +0100

    Add cruft as custom package

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,8 +1,12 @@
 # Custom packages, that can be defined similarly to ones from nixpkgs
 # You can build them using 'nix build .#example'
-pkgs: {
-  my = {
+pkgs: let
+  callPythonPackage = pkgs.lib.callPackageWith (pkgs // pkgs.python3Packages // {pydantic_1 = pkgs.python3Packages.pydantic;});
+in {
+  my = rec {
     # example = pkgs.callPackage ./example { };
     gh-copilot = pkgs.callPackage ./gh-copilot.nix {};
+    cruft = callPythonPackage ./cruft.nix {inherit examples;};
+    examples = callPythonPackage ./python-examples {};
   };
 }
