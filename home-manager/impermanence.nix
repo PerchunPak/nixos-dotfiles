eb649e16ccdf6fc9b67eef8d1ff137535b388aff
PerchunPak <git@perchun.it>
2024-03-31 19:29:59 +0200

    Save undo history and MRU of opened files in VIM between reboots

--- home-manager/impermanence.nix
+++ home-manager/impermanence.nix
@@ -15,6 +15,8 @@
       ".local/share/pypoetry"
       ".cache/pypoetry"
       ".local/share/PrismLauncher"
+      ".local/state/nvim/shada" # MRU on default screen
+      ".local/state/nvim/undo" # Undo history
       {
         directory = ".local/share/Steam";
         method = "symlink";
