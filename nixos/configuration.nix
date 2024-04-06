b56343354b419a70f3fa6199de29627b781713c0
PerchunPak <git@perchun.it>
2024-04-06 10:03:45 +0200

    Add Nvidia proprietary drivers

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -27,6 +27,7 @@
     ./impermanence.nix
     ./catppuccin.nix
     ./apple-keyboard.nix
+    ./nvidia.nix
 
     # Import your generated (nixos-generate-config) hardware configuration
     ./hardware-configuration.nix
