f0f70ea1275b1f4798811583449f6c0d99d5c498
PerchunPak <git@perchun.it>
2024-04-07 11:03:19 +0200

    Add workaround for crashing session on auto-login

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -107,6 +107,11 @@
     Defaults        lecture=never
   '';
 
+  # Workaround for crashing session on auto-login
+  # see https://github.com/NixOS/nixpkgs/issues/103746
+  systemd.services."getty@tty1".enable = false;
+  systemd.services."autovt@tty1".enable = false;
+
   programs.fish.enable = true; # for making it default shell
 
   users.users = {
