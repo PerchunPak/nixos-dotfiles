b868a7fe583bcf7372429432998defd8a1799c44
PerchunPak <git@perchun.it>
2024-04-16 19:27:25 +0200

    Add nix-ld

---
+++ modules/nixos/nix-ld/default.nix
@@ -0,0 +1,12 @@
+{
+  pkgs,
+  options,
+  ...
+}: {
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries =
+    options.programs.nix-ld.libraries.default
+    ++ (with pkgs; [
+      e2fsprogs
+    ]);
+}
