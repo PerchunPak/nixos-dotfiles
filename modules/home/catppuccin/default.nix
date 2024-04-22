de73c8d19312c212c067e780d3163b339c8f63c5
PerchunPak <git@perchun.it>
2024-04-22 23:05:27 +0200

    Do not provide an option to disable catppuccin

--- modules/home/catppuccin/default.nix
+++ modules/home/catppuccin/default.nix
@@ -1,31 +1,13 @@
-{
-  inputs,
-  config,
-  lib,
-  ...
-}: let
-  cfg = config.my.catppuccin;
-  # https://github.com/catppuccin/nix/blob/main/modules/lib/default.nix#L24-L38
-  # or permalink https://github.com/catppuccin/nix/blob/03b95cad3bbeb9913db6d89dc3f4fccc6c8fcbd4/modules/lib/default.nix#L24-L38
-  accent = "maroon";
-in {
+{inputs, ...}: {
   imports = [
     inputs.catppuccin.homeManagerModules.catppuccin
   ];
 
-  options = {
-    my.catppuccin = {
-      enable = lib.my.mkEnableByDefaultOption "auto-theming catppuccin";
-    };
+  catppuccin = {
+    enable = true;
+    accent = "maroon";
+    flavour = "mocha";
   };
 
-  config = lib.mkIf cfg.enable {
-    catppuccin = {
-      enable = true;
-      accent = "maroon";
-      flavour = "mocha";
-    };
-
-    gtk.catppuccin.cursor.accent = "dark";
-  };
+  gtk.catppuccin.cursor.accent = "dark";
 }
