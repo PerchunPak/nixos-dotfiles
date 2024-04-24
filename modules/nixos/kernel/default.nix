adf79b5b346d5524efbb13f63eb8795743a67e73
PerchunPak <git@perchun.it>
2024-04-24 22:15:17 +0200

    Revert "Finally fix sleep"
    This reverts commit 30e56e64457ed9543d48b1647ec198fee6f0021d.
    
    Everything only got worse

--- modules/nixos/kernel/default.nix
+++ modules/nixos/kernel/default.nix
@@ -2,4 +2,11 @@
 {
   # use latest kernel because i can
   boot.kernelPackages = pkgs.linuxPackages_zen;
+
+  boot.kernelParams = [
+    "nvidia"
+    "nvidia_modeset"
+    "nvidia_uvm"
+    "nvidia_drm"
+  ];
 }
