30e56e64457ed9543d48b1647ec198fee6f0021d
PerchunPak <git@perchun.it>
2024-04-24 10:31:43 +0200

    Finally fix sleep

--- modules/nixos/kernel/default.nix
+++ modules/nixos/kernel/default.nix
@@ -2,11 +2,4 @@
 {
   # use latest kernel because i can
   boot.kernelPackages = pkgs.linuxPackages_zen;
-
-  boot.kernelParams = [
-    "nvidia"
-    "nvidia_modeset"
-    "nvidia_uvm"
-    "nvidia_drm"
-  ];
 }
--- modules/nixos/nvidia/default.nix
+++ modules/nixos/nvidia/default.nix
@@ -37,7 +37,7 @@ in
       # Enable this if you have graphical corruption issues or application crashes after waking
       # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
       # of just the bare essentials.
-      powerManagement.enable = false;
+      powerManagement.enable = true;
 
       # Fine-grained power management. Turns off GPU when not in use.
       # Experimental and only works on modern Nvidia GPUs (Turing or newer).
@@ -54,7 +54,7 @@ in
 
       # Enable the Nvidia settings menu,
       # accessible via `nvidia-settings`.
-      nvidiaSettings = true;
+      nvidiaSettings = false;
 
       # Optionally, you may need to select the appropriate driver version for your specific GPU.
       package = cfg.package;
