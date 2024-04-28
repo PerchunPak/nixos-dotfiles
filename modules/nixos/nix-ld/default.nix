02be09367bfc4ff6ad2b41856df46be3420fd02e
PerchunPak <git@perchun.it>
2024-04-28 16:15:27 +0200

    Add libgcc to nix-ld

--- modules/nixos/nix-ld/default.nix
+++ modules/nixos/nix-ld/default.nix
@@ -1,7 +1,14 @@
 { pkgs, options, ... }:
 {
-  programs.nix-ld.enable = true;
-  programs.nix-ld.libraries = options.programs.nix-ld.libraries.default ++ (with pkgs; [ e2fsprogs ]);
+  programs.nix-ld = {
+    enable = true;
+    libraries =
+      options.programs.nix-ld.libraries.default
+      ++ (with pkgs; [
+        e2fsprogs
+        libgcc
+      ]);
+  };
 
   environment.systemPackages = with pkgs; [ steam-run ];
 }
