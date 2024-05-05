3b924c563a14ccd7dccfef8a6d54a7f91b400c8e
PerchunPak <git@perchun.it>
2024-05-05 14:10:48 +0200

    Install java globally if installing Minecraft

--- modules/home/gaming/minecraft/default.nix
+++ modules/home/gaming/minecraft/default.nix
@@ -21,6 +21,10 @@ in
       # not settings this may delete my data, better to keep empty symlink
       my.persistence.directories = [ ".local/share/PrismLauncher" ];
     }
-    (lib.mkIf cfg.minecraft.enable { home.packages = with pkgs; [ prismlauncher ]; })
+    (lib.mkIf cfg.minecraft.enable {
+      home.packages = with pkgs; [ prismlauncher ];
+
+      programs.java.enable = true;
+    })
   ];
 }
