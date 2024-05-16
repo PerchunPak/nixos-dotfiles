3af002fd7817ec1fed2c328c2be8ab58ed413273
PerchunPak <git@perchun.it>
2024-05-16 16:09:35 +0200

    Revert "Add plymouth"
    I don't like how it blocks my logs.
    
    This reverts commit 42cbcf79ca78546803d54185cd0d9515a64c49f6.

--- modules/nixos/basic/default.nix
+++ modules/nixos/basic/default.nix
@@ -17,19 +17,16 @@
     LC_TIME = "en_US.UTF-8";
   };
 
-  boot = {
-    plymouth.enable = true;
-    loader.grub = {
-      enable = true;
-      device = "nodev";
-      efiSupport = true;
-      useOSProber = true;
-      extraConfig = ''
-        GRUB_CMDLINE_LINUX_DEFAULT="nvidia-drm.modeset=1"
-      '';
-    };
-    loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub = {
+    enable = true;
+    device = "nodev";
+    efiSupport = true;
+    useOSProber = true;
+    extraConfig = ''
+      GRUB_CMDLINE_LINUX_DEFAULT="nvidia-drm.modeset=1"
+    '';
   };
+  boot.loader.efi.canTouchEfiVariables = true;
 
   programs.nix-ld.enable = true;
 }
