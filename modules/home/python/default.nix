4c541eaee457967a9ad1a632da4738885a140bf5
PerchunPak <git@perchun.it>
2024-05-27 21:05:26 +0200

    Use system python as default in poetry

--- modules/home/python/default.nix
+++ modules/home/python/default.nix
@@ -5,9 +5,21 @@
     python312Packages.black
     python312Packages.ipython
     ruff
-    poetry
     pkgs.my.cruft
   ];
+
+  programs = {
+    poetry = {
+      enable = true;
+      settings = {
+        # use python 3.12 instead of what nixos installs poetry with (3.11)
+        #
+        # i wish there was a way to override poetry in that way, it could
+        # be built on 3.12
+        virtualenvs.prefer-active-python = true;
+      };
+    };
+  };
 
   my.persistence.directories = [
     ".local/share/pypoetry"
