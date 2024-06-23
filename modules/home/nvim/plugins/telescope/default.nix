a0fb6c8a48468e93bb0bfde07e634f3468665ee0
PerchunPak <git@perchun.it>
2024-06-23 09:43:03 +0200

    Properly add keymaps for fuzzy find in current file

--- modules/home/nvim/plugins/telescope/default.nix
+++ modules/home/nvim/plugins/telescope/default.nix
@@ -91,17 +91,10 @@
     extraConfigLua = ''
       local builtin = require("telescope.builtin")
 
-      -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set("n", "<leader>/", function()
-        -- You can pass additional configuration to telescope to change theme, layout, etc.
-        builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
-          winblend = 10,
-          previewer = false,
-        }))
+        builtin.current_buffer_fuzzy_find({ previewer = false }))
       end, { desc = "[/] Fuzzily search in current file" })
 
-      -- Also possible to pass additional configuration options.
-      --  See `:help telescope.builtin.live_grep()` for information about particular keys
       vim.keymap.set("n", "<leader>s/", function()
         builtin.live_grep({
           grep_open_files = true,
