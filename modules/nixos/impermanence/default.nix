251739a3d198e5ca006ba0dc149e0adcedfe4fc1
PerchunPak <git@perchun.it>
2024-07-03 11:31:50 +0200

    Syntax highlight my impermanence script

--- modules/nixos/impermanence/default.nix
+++ modules/nixos/impermanence/default.nix
@@ -24,30 +24,32 @@ in
   };
 
   config = {
-    boot.initrd.postDeviceCommands = lib.mkAfter ''
-      mkdir /btrfs_tmp
-      mount /dev/mapper/root_vg-root /btrfs_tmp
-      if [[ -e /btrfs_tmp/root ]]; then
-          mkdir -p /btrfs_tmp/old_roots
-          timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
-          mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
-      fi
+    boot.initrd.postDeviceCommands =
+      lib.mkAfter # bash
+        ''
+          mkdir /btrfs_tmp
+          mount /dev/mapper/root_vg-root /btrfs_tmp
+          if [[ -e /btrfs_tmp/root ]]; then
+            mkdir -p /btrfs_tmp/old_roots
+            timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
+            mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
+          fi
 
-      delete_subvolume_recursively() {
-          IFS=$'\n'
-          for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
+          delete_subvolume_recursively() {
+            IFS=$'\n'
+            for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
               delete_subvolume_recursively "/btrfs_tmp/$i"
+            done
+            btrfs subvolume delete "$1"
+          }
+
+          for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
+            delete_subvolume_recursively "$i"
           done
-          btrfs subvolume delete "$1"
-      }
 
-      for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
-          delete_subvolume_recursively "$i"
-      done
-
-      btrfs subvolume create /btrfs_tmp/root
-      umount /btrfs_tmp
-    '';
+          btrfs subvolume create /btrfs_tmp/root
+          umount /btrfs_tmp
+        '';
 
     fileSystems."/persist".neededForBoot = true;
     environment.persistence."/persist/system" = {
