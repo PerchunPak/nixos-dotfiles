7dd936ec9c95f3e1ee094ec3786c56d247bb02d5
PerchunPak <git@perchun.it>
2024-07-03 16:03:32 +0200

    Add notification when backup service crashes

--- modules/nixos/btrfs-snapshots/default.nix
+++ modules/nixos/btrfs-snapshots/default.nix
@@ -48,9 +48,23 @@
           script = "btrfs-backup --label ${variation.name} --keep ${toString variation.toKeep} -- ${all-subvolumes}";
           serviceConfig.User = "root";
           startAt = if variation.systemdFormat != null then variation.systemdFormat else variation.name;
+          onFailure = [ "btrfs-backup-on-failure.service" ];
         };
       }) variations
-    );
+    )
+
+    // {
+      btrfs-backup-on-failure = {
+        environment = {
+          DISPLAY = ":0";
+          DBUS_SESSION_BUS_ADDRESS = "unix:path=/run/user/1000/bus";
+        };
+
+        script = "${pkgs.libnotify}/bin/notify-send 'One of auto-backup scripts just crashed!'";
+        serviceConfig.Type = "oneshot";
+        serviceConfig.User = "perchun";
+      };
+    };
 
   my.persistence.directories = [ "/.btrfs" ];
 }
