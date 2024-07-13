6fd23f1339bdb0964622121d4fe5d21237513084
PerchunPak <git@perchun.it>
2024-07-13 18:43:28 +0200

    Always enable polkit

--- modules/nixos/basic/default.nix
+++ modules/nixos/basic/default.nix
@@ -32,6 +32,8 @@
 
   networking.networkmanager.enable = true;
 
+  security.polkit.enable = true;
+
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "23.11";
 }
