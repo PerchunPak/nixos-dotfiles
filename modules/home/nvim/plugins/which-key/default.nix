dc09e24190e84d8bfef26b0ef408ef6963a1e34e
PerchunPak <git@perchun.it>
2024-07-25 15:17:39 +0200

    Disable registrations in which-key.nvim

--- modules/home/nvim/plugins/which-key/default.nix
+++ modules/home/nvim/plugins/which-key/default.nix
@@ -2,28 +2,30 @@
   programs.nixvim.plugins = {
     which-key = {
       enable = true;
-      registrations = {
-        "<leader>c" = {
-          name = "[C]ode";
-          _ = "which_key_ignore";
-        };
-        "<leader>d" = {
-          name = "[D]ocument";
-          _ = "which_key_ignore";
-        };
-        "<leader>r" = {
-          name = "[R]ename";
-          _ = "which_key_ignore";
-        };
-        "<leader>s" = {
-          name = "[S]earch";
-          _ = "which_key_ignore";
-        };
-        "<leader>w" = {
-          name = "[W]orkspace";
-          _ = "which_key_ignore";
-        };
-      };
+      # TODO: re-enable when https://github.com/nix-community/nixvim/pull/1927 will be merged
+
+      # registrations = {
+      #   "<leader>c" = {
+      #     name = "[C]ode";
+      #     _ = "which_key_ignore";
+      #   };
+      #   "<leader>d" = {
+      #     name = "[D]ocument";
+      #     _ = "which_key_ignore";
+      #   };
+      #   "<leader>r" = {
+      #     name = "[R]ename";
+      #     _ = "which_key_ignore";
+      #   };
+      #   "<leader>s" = {
+      #     name = "[S]earch";
+      #     _ = "which_key_ignore";
+      #   };
+      #   "<leader>w" = {
+      #     name = "[W]orkspace";
+      #     _ = "which_key_ignore";
+      #   };
+      # };
     };
   };
 }
