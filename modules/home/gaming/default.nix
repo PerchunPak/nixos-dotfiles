8aeb361bf23845ea695b7a457a66c7006bb745ed
PerchunPak <git@perchun.it>
2024-07-26 19:38:30 +0200

    Add heroic config to persistence

--- modules/home/gaming/default.nix
+++ modules/home/gaming/default.nix
@@ -21,7 +21,10 @@ in
   config = lib.mkMerge [
     {
       # not settings this may delete my data, better to keep empty symlink
-      my.persistence.directories = [ ".factorio" ];
+      my.persistence.directories = [
+        ".factorio"
+        ".config/heroic"
+      ];
     }
     (lib.mkIf cfg.enable {
       home.packages =
