44ac74c35e8523a77fbc523a169894537c240c20
PerchunPak <git@perchun.it>
2024-08-01 15:37:33 +0200

    Remove nixpkgs-small channel

--- flake.nix
+++ flake.nix
@@ -4,7 +4,6 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.11";
-    nixpkgs-small.url = "github:nixos/nixpkgs/nixos-unstable-small";
 
     home-manager = {
       url = "github:nix-community/home-manager";
