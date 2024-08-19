410ec2359cb1993830fc6f4df50658579e666917
PerchunPak <git@perchun.it>
2024-08-19 11:02:10 +0200

    nvim: fix toggleterm not opening

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -2,6 +2,7 @@ return {
   'akinsho/toggleterm.nvim',
   version = '*',
   opts = {
+    open_mapping = [[<c-\>]],
     shade_terminals = false,
     terminal_mappings = true,
     -- for system reproducibility reasons
