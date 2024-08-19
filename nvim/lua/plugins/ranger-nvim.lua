71df0a3973591334da866d0bcf0357d50239201c
PerchunPak <git@perchun.it>
2024-08-19 10:17:28 +0200

    nvim: rebind ranger-nvim to leader R
    it conflicts with `format buffer` keybind

--- nvim/lua/plugins/ranger-nvim.lua
+++ nvim/lua/plugins/ranger-nvim.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     require('ranger-nvim').setup { replace_netrw = true }
 
-    vim.api.nvim_set_keymap('n', '<leader>f', '', {
+    vim.api.nvim_set_keymap('n', '<leader>R', '', {
       noremap = true,
       callback = function()
         require('ranger-nvim').open(true)
