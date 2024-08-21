789ba17cc3ff0c20fd9e60f66e58f5d6c7b77b91
PerchunPak <git@perchun.it>
2024-08-21 12:05:25 +0200

    nvim: disable Sync clipboard between OS and Neovim

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -15,14 +15,6 @@ vim.opt.mouse = ''
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
 
--- Sync clipboard between OS and Neovim.
---  Schedule the setting after `UiEnter` because it can increase startup-time.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
-vim.schedule(function()
-  vim.opt.clipboard = 'unnamedplus'
-end)
-
 -- Enable break indent
 vim.opt.breakindent = true
 
