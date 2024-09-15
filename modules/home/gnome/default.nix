833535c623ecdd3966d808fad4c936065924d258
PerchunPak <git@perchun.it>
2024-09-15 12:16:40 +0200

    Fix warning

--- modules/home/gnome/default.nix
+++ modules/home/gnome/default.nix
@@ -8,8 +8,8 @@
   ];
 
   # alert sound
-  home.file.".local/share/sounds/__custom/bell-terminal.ogg".source = "${pkgs.gnome.gnome-control-center}/share/sounds/gnome/default/alerts/hum.ogg";
-  home.file.".local/share/sounds/__custom/bell-window-system.ogg".source = "${pkgs.gnome.gnome-control-center}/share/sounds/gnome/default/alerts/hum.ogg";
+  home.file.".local/share/sounds/__custom/bell-terminal.ogg".source = "${pkgs.gnome-control-center}/share/sounds/gnome/default/alerts/hum.ogg";
+  home.file.".local/share/sounds/__custom/bell-window-system.ogg".source = "${pkgs.gnome-control-center}/share/sounds/gnome/default/alerts/hum.ogg";
 
   my.persistence = {
     directories = [ ".local/share/keyrings" ];
