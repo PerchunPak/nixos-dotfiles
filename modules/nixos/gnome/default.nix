9f755b23d4115e015e61322fd23e1f9072c2b7da
PerchunPak <git@perchun.it>
2024-10-05 17:40:30 +0200

    Fix gdm crash on boot

--- modules/nixos/gnome/default.nix
+++ modules/nixos/gnome/default.nix
@@ -1,5 +1,10 @@
 { pkgs, ... }:
 {
+  # Workaround for crashing session on auto-login
+  # see https://github.com/NixOS/nixpkgs/issues/103746
+  systemd.services."getty@tty1".enable = false;
+  systemd.services."autovt@tty1".enable = false;
+
   programs.dconf.enable = true;
 
   environment.systemPackages = with pkgs; [
