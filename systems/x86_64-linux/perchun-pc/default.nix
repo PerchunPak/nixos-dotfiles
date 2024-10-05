1d2046f542ea82da01ab2b0efcd8ca595a18d342
PerchunPak <git@perchun.it>
2024-10-05 15:05:25 +0200

    Someone punched my monitor...

--- systems/x86_64-linux/perchun-pc/default.nix
+++ systems/x86_64-linux/perchun-pc/default.nix
@@ -11,9 +11,6 @@
     user.password = "$y$j9T$K1EYVhXG6o2yQPH4IUDsw0$P8nQZaE554VdULdAdPmJ2Z9S/fwQ132V6oYiM.RGZWB";
     nvidia.enable = true;
     zerotier.networks = [ "8850338390436420" ];
-    hyprland.monitors = [
-      "DP-1,2560x1440@164,1920x0,1"
-      "HDMI-A-2,1920x1080@60,0x556,1"
-    ];
+    hyprland.monitors = [ "HDMI-A-2,1920x1080@60,0x0,1" ];
   };
 }
