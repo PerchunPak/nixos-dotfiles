949c605deb2d87050da9b438807d15507e541e82
PerchunPak <git@perchun.it>
2024-10-12 13:29:02 +0200

    Build last dev version of Lix
    Because I can

--- flake.nix
+++ flake.nix
@@ -37,9 +37,14 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    lix = {
+      url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
+      flake = false;
+    };
     lix-module = {
       url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.lix.follows = "lix";
     };
 
     # Neovim
