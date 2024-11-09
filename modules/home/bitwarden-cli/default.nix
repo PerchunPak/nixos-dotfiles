a77cc6f9529f125fcd4bdff8545db8dc07b13a14
PerchunPak <git@perchun.it>
2024-11-09 12:10:39 +0100

    Remove unnecessary comment

--- modules/home/bitwarden-cli/default.nix
+++ modules/home/bitwarden-cli/default.nix
@@ -4,7 +4,6 @@
     enable = true;
     settings = {
       base_url = "https://bw.perchun.it";
-      # identity_url = "https://bw.perchun.it/identity";
       email = "bw@perchun.it";
       pinentry = pkgs.pinentry-rofi;
     };
