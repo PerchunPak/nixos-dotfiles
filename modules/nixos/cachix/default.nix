ba28310f7b6f294e5af664b4bfb6e7b3b63d0535
PerchunPak <git@perchun.it>
2024-11-16 12:05:28 +0100

    Remove COSMIC cachix
    I don't plan on trying COSMIC anymore

--- modules/nixos/cachix/default.nix
+++ modules/nixos/cachix/default.nix
@@ -4,12 +4,10 @@
     # Add cache for nix-community packages
     substituters = [
       "https://nix-community.cachix.org"
-      "https://cosmic.cachix.org"
     ];
 
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
     ];
   };
 }
