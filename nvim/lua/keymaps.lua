b8c371ed4d8f87566eb329a716fda88ee615688f
PerchunPak <git@perchun.it>
2024-11-20 18:05:08 +0100

    nvim: fix undotree keybind

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -73,4 +73,11 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+vim.keymap.set(
+  'n',
+  '<leader>u',
+  vim.cmd.UndotreeToggle,
+  { desc = 'Open [U]ndo tree' }
+)
+
 -- vim: ts=2 sts=2 sw=2 et
--- nvim/lua/plugins/undotree.lua
+++ nvim/lua/plugins/undotree.lua
@@ -4,14 +4,5 @@ return {
   config = function()
     vim.g.undotree_DiffpanelHeight = 0
     vim.g.undotree_SetFocusWhenToggle = 1
-
-    vim.keymap.set(
-      'n',
-      '<leader>u',
-      vim.cmd.UndotreeToggle,
-      { options = {
-        desc = 'Open [U]ndo tree',
-      } }
-    )
   end,
 }
