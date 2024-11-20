62d9fabeffbf0500e0fe254cdb3db9d05f105ec9
PerchunPak <git@perchun.it>
2024-11-20 12:43:59 +0100

    Install latest NUR and overlay the broken package

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nur.url = "github:nix-community/NUR?rev=83139931be39e5cb4deb85f3a86ff17d04a12928";
+    nur.url = "github:nix-community/NUR";
 
     agenix = {
       url = "github:ryantm/agenix";
