9221f7fe2d91c1a4479769729329a120bc845635
PerchunPak <git@perchun.it>
2024-11-29 04:03:22 +0100

    doublepress.sh: fix LSP warnings

--- packages/doublepress/doublepress.sh
+++ packages/doublepress/doublepress.sh
@@ -2,7 +2,7 @@
 # Credits: https://github.com/stackcoder/doublepress
 set -euo pipefail
 
-name="$(basename $BASH_SOURCE)"
+name="$(basename "${BASH_SOURCE[0]}")"
 lock="/var/run/${name}.lock"
 fifo="/var/run/${name}.fifo"
 #lock="./${name}.lock"
@@ -22,12 +22,12 @@ echo $$ 1>&200
 rm -f "${fifo}" && mkfifo "${fifo}"
 
 # cleanup fifo and lock file on exit
-trap "rm \"${fifo}\" && rm \"${lock}\"" EXIT
+trap 'rm "${fifo}" && rm "${lock}"' EXIT
 
 # wait for slave instances signaling button press
 counter=1
-for i in {1..10}; do
-  if read -t 0.5 signal; then
+for _ in {1..10}; do
+  if read -rt 0.5 _; then
     echo -n '+' && ((counter+=1))
   else
     echo -n '.'
