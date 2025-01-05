08c5b0a1d18ea35764451bd7d2aa1b51e1206080
PerchunPak <git@perchun.it>
2025-01-05 13:02:13 +0100

    nvim: fix lazy-nvim being redownloaded on every boot

--- nvim/lua/lazy-plugins.lua
+++ nvim/lua/lazy-plugins.lua
@@ -12,27 +12,33 @@ local function getlockfilepath()
   end
 end
 
-require('nixCatsUtils.lazyCat').setup(nixLazyPath, 'plugins', {
-  lockfile = getlockfilepath(),
-  ui = {
-    -- If you are using a Nerd Font: set icons to an empty table which will use the
-    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
-    icons = vim.g.have_nerd_font and {} or {
-      cmd = '⌘',
-      config = '🛠',
-      event = '📅',
-      ft = '📂',
-      init = '⚙',
-      keys = '🗝',
-      plugin = '🔌',
-      runtime = '💻',
-      require = '🌙',
-      source = '📄',
-      start = '🚀',
-      task = '📌',
-      lazy = '💤 ',
+-- NOTE: nixCats: this the lazy wrapper. Use it like require('lazy').setup() but with an extra
+-- argument, the path to lazy.nvim as downloaded by nix, or nil, before the normal arguments.
+require('nixCatsUtils.lazyCat').setup(
+  nixCats.pawsible { 'allPlugins', 'start', 'lazy.nvim' },
+  'plugins',
+  {
+    lockfile = getlockfilepath(),
+    ui = {
+      -- If you are using a Nerd Font: set icons to an empty table which will use the
+      -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+      icons = vim.g.have_nerd_font and {} or {
+        cmd = '⌘',
+        config = '🛠',
+        event = '📅',
+        ft = '📂',
+        init = '⚙',
+        keys = '🗝',
+        plugin = '🔌',
+        runtime = '💻',
+        require = '🌙',
+        source = '📄',
+        start = '🚀',
+        task = '📌',
+        lazy = '💤 ',
+      },
     },
-  },
-})
+  }
+)
 
 -- vim: ts=2 sts=2 sw=2 et
