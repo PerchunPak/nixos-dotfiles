bc787188a5561b3174f1e5c51c78fb6c106ba814
PerchunPak <git@perchun.it>
2025-01-12 11:39:59 +0100

    Fix build

--- nvim/default.nix
+++ nvim/default.nix
@@ -39,15 +39,6 @@ let
             (utils.standardPluginOverlay inputs)
             # add any flake overlays here.
             (final: prev: {
-              gleam = prev.gleam.overrideAttrs (oa: {
-                patches = (oa.patches or [ ]) ++ [
-                  (final.fetchpatch {
-                    url = "https://patch-diff.githubusercontent.com/raw/gleam-lang/gleam/pull/3855.patch?full_index=1";
-                    hash = "sha256-Kejh8fWhsMS3jLZIDBys1h4Xo4RbL8Axg0punoVFlsU=";
-                    excludes = [ "CHANGELOG.md" ];
-                  })
-                ];
-              });
             })
           ];
       in
@@ -89,12 +80,12 @@ let
             yaml-language-server
 
             # formatting
-            nixfmt-rfc-style
-            codespell
-            nodePackages.prettier
             black
-            stylua
+            codespell
             gawk # trim_whitespace
+            nixfmt-rfc-style
+            nodePackages.prettier
+            stylua
           ]
           ++ (with python3Packages; [
             debugpy
