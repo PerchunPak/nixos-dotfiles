0263f6e0473873fb6d5de0cd7083741f06f4d93d
PerchunPak <git@perchun.it>
2025-01-15 19:05:23 +0100

    Install darkreader

--- modules/home/firefox/default.nix
+++ modules/home/firefox/default.nix
@@ -13,6 +13,7 @@
       perchun = {
         extensions = with pkgs.nur.repos.rycee.firefox-addons; [
           bitwarden
+          darkreader
           disable-javascript
           enhancer-for-youtube
           grammarly
