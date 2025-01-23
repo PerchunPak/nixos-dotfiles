b0450526167bf09c4b6bd94751e44149faf80c0b
PerchunPak <git@perchun.it>
2025-01-23 09:47:28 +0100

    Add Lix binary cache

--- modules/nixos/cachix/default.nix
+++ modules/nixos/cachix/default.nix
@@ -5,10 +5,12 @@
     substituters = lib.mkForce [
       "https://nixos-cache-proxy.cofob.dev"
       "https://nix-community.cachix.org"
+      "https://cache.lix.systems"
     ];
 
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
     ];
   };
 }
