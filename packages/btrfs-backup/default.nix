64ef4fb9997bbbdf37c936794d326c356010daf5
PerchunPak <git@perchun.it>
2025-02-03 10:07:44 +0100

    Update btrfs-backup script

--- packages/btrfs-backup/default.nix
+++ packages/btrfs-backup/default.nix
@@ -2,8 +2,8 @@
 stdenv.mkDerivation {
   name = "btrfs-backup";
   src = fetchurl {
-    url = "https://raw.githubusercontent.com/ItsDrike/dotfiles/11ef5e8a1ca4f2705659844df84df848f7e2446e/root/usr/local/bin/btrfs-backup";
-    hash = "sha256-ZHyijplgFrf56FYz3MSh+P9kOVLZYQKUlrcxf0uWPj8=";
+    url = "https://raw.githubusercontent.com/ItsDrike/dotfiles/b93efba0fd22721be663e51c7c7d49cbc0d7a892/root/usr/local/bin/btrfs-backup";
+    hash = "sha256-0qLJPRNKASHT3EN7RvIauMFg00/j/ujokpZYY/dSevw=";
   };
   dontUnpack = true;
   installPhase = ''
