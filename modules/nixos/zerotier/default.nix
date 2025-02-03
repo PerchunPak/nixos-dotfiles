618698b144d35e22f783823e5497f537d20d932d
PerchunPak <git@perchun.it>
2025-02-03 20:23:46 +0100

    Disable zerotier
    I don't use it rn

--- modules/nixos/zerotier/default.nix
+++ modules/nixos/zerotier/default.nix
@@ -5,7 +5,7 @@ in
 {
   options = {
     my.zerotier = {
-      enable = lib.my.mkEnableByDefaultOption "zerotier";
+      enable = lib.my.mkEnableOption "zerotier";
       networks = lib.mkOption { type = lib.types.listOf lib.types.str; };
     };
   };
--- systems/x86_64-linux/perchun-pc/default.nix
+++ systems/x86_64-linux/perchun-pc/default.nix
@@ -10,7 +10,6 @@
   my = {
     user.password = "$y$j9T$bLmizdUJczqBOKxdu/pC40$d4nIu4bEfXL4FtnuZnS92LEfXdM8kgMidPHMX.hoLuD";
     nvidia.enable = true;
-    zerotier.networks = [ "8850338390436420" ];
     hyprland.monitors = [ "HDMI-A-2,1920x1080@60,0x0,1" ];
   };
 }
