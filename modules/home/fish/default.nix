f2bcd904f3a442beefb16b40e20bc3e68de3304f
PerchunPak <git@perchun.it>
2025-02-04 10:12:36 +0100

    Move various scripts from fish aliases to packages

--- modules/home/fish/default.nix
+++ modules/home/fish/default.nix
@@ -15,18 +15,6 @@
       la = "ls -la";
       lt = "la --tree";
       kssh = "kitten ssh";
-
-      rebuild =
-        (pkgs.writeShellScript "rebuild.sh" ''
-          set -ex
-          trap 'cd -' EXIT
-
-          cd ~/dotfiles
-          git add .
-          sudo true
-          nh os switch $@
-        '').outPath;
-
       rebuildt = "rebuild -- --show-trace --option eval-cache false";
       mount-diskroot = "sudo mkdir /disk-root && sudo mount /dev/root_vg/root /disk-root";
       pystart = "source ~/dev/python-template/.venv/bin/activate.fish && cruft create ~/dev/python-template && deactivate";
@@ -34,44 +22,7 @@
       mtr = "mtr --order 'LSD   NBAW'";
       gh = "GITHUB_TOKEN=(rbw get 'GitHub CLI token') ${pkgs.gh}/bin/gh";
       nixpkgs-review = "GITHUB_TOKEN=(rbw get 'GitHub CLI token') ${pkgs.nixpkgs-review}/bin/nixpkgs-review";
-      nreview =
-        (pkgs.writeShellScript "nreview.sh" ''
-          set -e
-          export GITHUB_TOKEN=$(rbw get 'GitHub CLI token')
-          set -x
-
-          trap 'cd -' EXIT
-          cd ~/dev/nixpkgs/master
-          nixpkgs-review pr --no-shell --print-result --systems "$@"
-        '').outPath;
-
-      modify =
-        (pkgs.writeShellScript "modify.sh" ''
-          set -ex
-          mv "$1" "$1"1
-          cat "$1"1 > "$1"
-        '').outPath;
-      modifyu =
-        (pkgs.writeShellScript "modify-undo.sh" ''
-          set -ex
-          mv "$1" "$1".modified
-          mv "$1"1 "$1"
-        '').outPath;
-
       list-generations = "sudo nix-env --list-generations --profile /nix/var/nix/profiles/system";
-      rollback =
-        (pkgs.writeShellScript "rollback.sh" ''
-          set -ex
-          sudo nix-env --switch-generation "$1" -p /nix/var/nix/profiles/system
-          sudo /nix/var/nix/profiles/system/bin/switch-to-configuration switch
-        '').outPath;
-
-      ght = # GitHub Token
-        (pkgs.writeShellScript "ght.sh" ''
-          set -e
-          export GITHUB_TOKEN=$(rbw get 'GitHub CLI token')
-          exec $@
-        '').outPath;
     };
 
     plugins = [
---
+++ modules/home/scripts/default.nix
@@ -0,0 +1,46 @@
+{ pkgs, ... }:
+{
+  home.packages = [
+    (pkgs.writeShellScriptBin "rebuild" ''
+      set -ex
+      trap 'cd -' EXIT
+
+      cd ~/dotfiles
+      git add .
+      sudo true
+      nh os switch "$@"
+    '')
+
+    (pkgs.writeShellScriptBin "nreview" ''
+      set -ex
+
+      trap 'cd -' EXIT
+      cd ~/dev/nixpkgs/master
+      ght nixpkgs-review pr --no-shell --print-result --systems "$@"
+    '')
+
+    (pkgs.writeShellScriptBin "modify" ''
+      set -ex
+      mv "$1" "$1"1
+      cat "$1"1 > "$1"
+    '')
+
+    (pkgs.writeShellScriptBin "modify-undo" ''
+      set -ex
+      mv "$1" "$1".modified
+      mv "$1"1 "$1"
+    '')
+
+    (pkgs.writeShellScriptBin "rollback" ''
+      set -ex
+      sudo nix-env --switch-generation "$1" -p /nix/var/nix/profiles/system
+      sudo /nix/var/nix/profiles/system/bin/switch-to-configuration switch
+    '')
+
+    (pkgs.writeShellScriptBin "ght" ''
+      set -e
+      export GITHUB_TOKEN=$(rbw get 'GitHub CLI token')
+      exec "$@"
+    '')
+  ];
+}
