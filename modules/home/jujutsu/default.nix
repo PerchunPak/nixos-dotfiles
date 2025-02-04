b8f67bf7a4d39ae8bd3dbd30af053796315540b7
PerchunPak <git@perchun.it>
2025-02-04 14:28:49 +0100

    Disable pagination in jj

--- modules/home/jujutsu/default.nix
+++ modules/home/jujutsu/default.nix
@@ -9,6 +9,8 @@ in
       user.name = git.userName;
       user.email = git.userEmail;
 
+      ui.paginate = "never";
+
       core.fsmonitor = "watchman";
       core.watchman.register_snapshot_trigger = true;
 
