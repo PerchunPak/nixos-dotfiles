aa7821695c8c76f5e37dd281c2a457f8ba479d8c
PerchunPak <git@perchun.it>
2025-02-12 15:54:15 +0100

    Use upstream's tldr-update module

--- modules/home/tldr/default.nix
+++ modules/home/tldr/default.nix
@@ -1,67 +1,9 @@
+{ pkgs, ... }:
 {
-  config,
-  lib,
-  pkgs,
-  ...
-}:
-let
-  cfg = config.programs.tldr;
-in
-{
-  imports = [ ./settings.nix ];
-
-  meta.maintainers = [ lib.maintainers.perchun ];
-
-  options.programs.tldr = {
-    enable = lib.mkEnableOption "TLDR CLI";
-
-    package = lib.mkPackageOption pkgs "tldr" { example = "tlrc"; };
-
-    auto-updates = {
-      enable = lib.mkEnableOption "Auto updates" // {
-        default = true;
-      };
-
-      period = lib.mkOption {
-        type = lib.types.str;
-        default = "weekly";
-        description = ''
-          Systemd timer period to create for scheduled {command}`tldr --update`.
-        '';
-      };
-    };
+  services.tldr-update = {
+    enable = true;
+    package = pkgs.tealdeer;
   };
 
-  config = lib.mkMerge [
-    (lib.mkIf cfg.enable {
-      home.packages = [ cfg.package ];
-
-    })
-    (lib.mkIf cfg.auto-updates.enable {
-      systemd.user.services.tldr-update = {
-        Unit = {
-          Description = "Update tldr CLI cache";
-          Documentation = "https://tldr.sh/";
-        };
-
-        Service = {
-          Type = "oneshot";
-          ExecStart = ''
-            "${lib.getExe cfg.package}" --update
-          '';
-        };
-      };
-
-      systemd.user.timers.tldr-update = {
-        Unit.Description = "Update tldr CLI cache";
-
-        Timer = {
-          OnCalendar = cfg.auto-updates.period;
-          Persistent = true;
-        };
-
-        Install.WantedBy = [ "timers.target" ];
-      };
-    })
-  ];
+  my.persistence.directories = [ ".cache/tealdeer" ];
 }
--- modules/home/tldr/settings.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, ... }:
-{
-  programs.tldr = {
-    enable = true;
-    package = pkgs.tealdeer;
-  };
-
-  my.persistence.directories = [ ".cache/tlrc" ];
-}
