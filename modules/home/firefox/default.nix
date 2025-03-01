af60a6e5b5e55f3dd675914a6662462ad223d577
PerchunPak <git@perchun.it>
2025-03-01 07:07:03 +0100

    Start configuring firefox extensions through nix

--- modules/home/firefox/default.nix
+++ modules/home/firefox/default.nix
@@ -1,11 +1,34 @@
 {
   pkgs,
-  config,
   inputs,
   ...
 }:
 let
   rycee-nur = import "${inputs.rycee-nur}" { inherit pkgs; };
+
+  profile = {
+    extensions.packages = with rycee-nur.firefox-addons; [
+      bitwarden
+      darkreader
+      disable-javascript
+      enhancer-for-youtube
+      grammarly
+      languagetool
+      refined-github
+      simple-tab-groups
+      sponsorblock
+      stylus
+      ublacklist
+      ublock-origin
+      vimium
+    ];
+
+    settings = {
+      "extensions.webextensions.ExtensionStorageIDB.enabled" = false;
+      "browser.download.lastDir" = "/home/perchun/Downloads";
+      "browser.sessionstore.max_resumed_crashes" = -1;
+    };
+  };
 in
 {
   imports = [ ./ublock-origin.nix ];
@@ -13,29 +36,8 @@ in
   programs.firefox = {
     enable = true;
     profiles = {
-      perchun = {
-        extensions.packages = with rycee-nur.firefox-addons; [
-          bitwarden
-          darkreader
-          disable-javascript
-          enhancer-for-youtube
-          grammarly
-          languagetool
-          refined-github
-          simple-tab-groups
-          sponsorblock
-          stylus
-          ublacklist
-          ublock-origin
-          vimium
-        ];
-
-        settings = {
-          "browser.download.lastDir" = "/home/perchun/Downloads";
-          "browser.sessionstore.max_resumed_crashes" = -1;
-        };
-      };
-      stuff = config.programs.firefox.profiles.perchun // {
+      perchun = profile;
+      stuff = profile // {
         id = 1;
       };
     };
