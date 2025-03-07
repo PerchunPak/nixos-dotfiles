d60598f9ee53155ff40ef1cd4bb60fcdc4b925ff
PerchunPak <git@perchun.it>
2025-03-07 18:38:43 +0100

    Enable VMs

--- systems/x86_64-linux/perchun-laptop/default.nix
+++ systems/x86_64-linux/perchun-laptop/default.nix
@@ -8,6 +8,7 @@
   networking.hostName = "perchun-laptop";
 
   my = {
+    vm.enable = true;
     user.password = "$y$j9T$27S3SAmwSy9GYZyTvAVI//$kNaRPqAAe5two6jEJdvvg06zrEEJ66LSOh/wnWQtNmA";
     nvidia.enable = true;
     hyprland.monitors = [ "eDP-1,1920x1080@144,0x0,1" ];
