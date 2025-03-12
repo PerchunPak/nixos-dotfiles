9d8c9dce9691faf559306d43c2f8c539eb74b5e1
PerchunPak <git@perchun.it>
2025-03-12 07:33:38 +0100

    Return some folders that should be persistent

--- modules/home/impermanence/default.nix
+++ modules/home/impermanence/default.nix
@@ -15,4 +15,22 @@
       };
     };
   };
+
+  config = {
+    my.persistence = {
+      directories = [
+        ".cache/huggingface"
+        ".cache/nix"
+        ".config/asciinema"
+        ".local/share/Steam"
+        ".local/share/docker"
+        ".local/share/flatpak"
+        ".local/state/wireplumber"
+        "ai"
+        "dev"
+        "dotfiles"
+        "persistent"
+      ];
+    };
+  };
 }
