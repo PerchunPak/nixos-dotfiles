bd967e905883c119bfc86643625b4f4b61c5eef1
PerchunPak <git@perchun.it>
2025-03-14 10:01:50 +0100

    Let's try wayland wine

--- modules/home/gaming/default.nix
+++ modules/home/gaming/default.nix
@@ -31,7 +31,7 @@ in
       home.packages =
         with pkgs;
         [
-          wineWowPackages.full
+          wineWowPackages.waylandFull
           heroic
         ]
         ++ cfg.additional-games;
