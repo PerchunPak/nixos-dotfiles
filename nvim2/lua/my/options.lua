bdaf9f7e05ea0b2458f7ad7a5b335b5a45dcd8cf
PerchunPak <git@perchun.it>
2025-04-07 14:05:16 +0200

    nvim2: set leader key

--- nvim2/lua/my/options.lua
+++ nvim2/lua/my/options.lua
@@ -1,3 +1,12 @@
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = true
+
 -- Make line numbers default
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
