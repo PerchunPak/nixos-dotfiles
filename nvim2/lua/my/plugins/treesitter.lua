cd50eec40549a0b3b7790dd894cf553622ca3649
PerchunPak <git@perchun.it>
2025-04-07 14:10:29 +0200

    nvim2: add treesitter

---
+++ nvim2/lua/my/plugins/treesitter.lua
@@ -0,0 +1,16 @@
+require('lze').load {
+  {
+    'nvim-treesitter',
+    event = 'DeferredUIEnter',
+    load = function(name)
+      vim.cmd.packadd(name)
+      vim.cmd.packadd 'nvim-treesitter-textobjects'
+    end,
+    after = function(plugin)
+      require('nvim-treesitter.configs').setup {
+        highlight = { enable = true },
+        indent = { enable = false },
+      }
+    end,
+  },
+}
