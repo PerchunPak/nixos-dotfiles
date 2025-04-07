3821a3b70c1f7f162dc8669ab3e464d6dea66239
PerchunPak <git@perchun.it>
2025-04-07 15:18:16 +0200

    nvim2: econom one tab in blink's config

--- nvim2/lua/my/plugins/blink-cmp.lua
+++ nvim2/lua/my/plugins/blink-cmp.lua
@@ -1,57 +1,55 @@
 return {
-  {
-    'blink.cmp',
+  'blink.cmp',
 
-    after = function()
-      require('blink.cmp').setup {
-        -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-        -- 'super-tab' for mappings similar to vscode (tab to accept)
-        -- 'enter' for enter to accept
-        -- 'none' for no mappings
-        --
-        -- All presets have the following mappings:
-        -- C-space: Open menu or open docs if already open
-        -- C-n/C-p or Up/Down: Select next/previous item
-        -- C-e: Hide menu
-        -- C-k: Toggle signature help (if signature.enabled = true)
-        --
-        -- See :h blink-cmp-config-keymap for defining your own keymap
-        keymap = {
-          preset = 'enter',
+  after = function()
+    require('blink.cmp').setup {
+      -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+      -- 'super-tab' for mappings similar to vscode (tab to accept)
+      -- 'enter' for enter to accept
+      -- 'none' for no mappings
+      --
+      -- All presets have the following mappings:
+      -- C-space: Open menu or open docs if already open
+      -- C-n/C-p or Up/Down: Select next/previous item
+      -- C-e: Hide menu
+      -- C-k: Toggle signature help (if signature.enabled = true)
+      --
+      -- See :h blink-cmp-config-keymap for defining your own keymap
+      keymap = {
+        preset = 'enter',
 
-          ['<Tab>'] = { 'select_next', 'fallback' },
-          ['<S-Tab>'] = { 'select_prev', 'fallback' },
-        },
+        ['<Tab>'] = { 'select_next', 'fallback' },
+        ['<S-Tab>'] = { 'select_prev', 'fallback' },
+      },
 
-        appearance = {
-          -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-          -- Adjusts spacing to ensure icons are aligned
-          nerd_font_variant = 'mono',
-        },
+      appearance = {
+        -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+        -- Adjusts spacing to ensure icons are aligned
+        nerd_font_variant = 'mono',
+      },
 
-        completion = { documentation = { auto_show = true } },
+      completion = { documentation = { auto_show = true } },
 
-        signature = { enabled = true },
+      signature = { enabled = true },
 
-        -- Default list of enabled providers defined so that you can extend it
-        -- elsewhere in your config, without redefining it, due to `opts_extend`
-        sources = {
-          default = { 'lazydev', 'lsp', 'snippets', 'path', 'buffer' },
-          providers = {
-            lazydev = {
-              name = 'LazyDev',
-              module = 'lazydev.integrations.blink',
-              -- make lazydev completions top priority (see `:h blink.cmp`)
-              score_offset = 100,
-              enabled = function()
-                return vim.tbl_contains({ 'lua' }, vim.bo.filetype)
-              end,
-            },
+      -- Default list of enabled providers defined so that you can extend it
+      -- elsewhere in your config, without redefining it, due to `opts_extend`
+      sources = {
+        default = { 'lazydev', 'lsp', 'snippets', 'path', 'buffer' },
+        providers = {
+          lazydev = {
+            name = 'LazyDev',
+            module = 'lazydev.integrations.blink',
+            -- make lazydev completions top priority (see `:h blink.cmp`)
+            score_offset = 100,
+            enabled = function()
+              return vim.tbl_contains({ 'lua' }, vim.bo.filetype)
+            end,
           },
         },
+      },
 
-        fuzzy = { implementation = 'prefer_rust_with_warning' },
-      }
-    end,
-  },
+      fuzzy = { implementation = 'prefer_rust_with_warning' },
+    }
+  end,
 }
