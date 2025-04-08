85bfa59db8f9cdbc995454ddf9e68a19e63a8f85
PerchunPak <git@perchun.it>
2025-04-08 10:14:01 +0200

    nvim: reorder mini.nvim config

--- nvim/lua/my/plugins/mini.lua
+++ nvim/lua/my/plugins/mini.lua
@@ -2,19 +2,12 @@ return {
   'mini.nvim',
   lazy = false,
   after = function()
-    -- Better Around/Inside textobjects
-    --
-    -- Examples:
-    --  - va)  - [V]isually select [A]round [)]paren
-    --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-    --  - ci'  - [C]hange [I]nside [']quote
     require('mini.ai').setup { n_lines = 500 }
-
-    -- Add/delete/replace surroundings (brackets, quotes, etc.)
-    --
-    -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-    -- - sd'   - [S]urround [D]elete [']quotes
-    -- - sr)'  - [S]urround [R]eplace [)] [']
+    require('mini.comment').setup {}
+    require('mini.cursorword').setup {}
+    require('mini.icons').setup {}
+    require('mini.move').setup {}
+    require('mini.pairs').setup {}
     require('mini.surround').setup()
 
     -- Simple and easy statusline.
@@ -31,11 +24,5 @@ return {
     statusline.section_location = function()
       return '%2l:%-2v'
     end
-
-    require('mini.comment').setup {}
-    require('mini.cursorword').setup {}
-    require('mini.icons').setup {}
-    require('mini.move').setup {}
-    require('mini.pairs').setup {}
   end,
 }
