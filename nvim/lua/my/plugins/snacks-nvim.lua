310e227bc46f270508b72a746515326bf1ac4f4d
PerchunPak <git@perchun.it>
2025-04-11 12:14:21 +0200

    nvim: tweak keymaps

--- nvim/lua/my/plugins/snacks-nvim.lua
+++ nvim/lua/my/plugins/snacks-nvim.lua
@@ -114,7 +114,7 @@ return {
     -- LSP
     { "gd",              function() Snacks.picker.lsp_definitions() end,                         desc = "Goto Definition" },
     { "gD",              function() Snacks.picker.lsp_declarations() end,                        desc = "Goto Declaration" },
-    { "gr",              function() Snacks.picker.lsp_references() end,                          nowait = true,                     desc = "References" },
+    { "gr",              function() Snacks.picker.lsp_references() end,                          desc = "References" ,              nowait = true },
     { "gI",              function() Snacks.picker.lsp_implementations() end,                     desc = "Goto Implementation" },
     { "gy",              function() Snacks.picker.lsp_type_definitions() end,                    desc = "Goto T[y]pe Definition" },
     { "<leader>ss",      function() Snacks.picker.lsp_symbols() end,                             desc = "LSP Symbols" },
