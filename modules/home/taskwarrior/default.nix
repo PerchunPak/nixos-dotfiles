a2b134e523a1d686f77086ce3a6fdb5abf4d6396
PerchunPak <git@perchun.it>
2025-04-15 01:20:34 +0200

    taskwarrior: make lower priority for "low" priority

--- modules/home/taskwarrior/default.nix
+++ modules/home/taskwarrior/default.nix
@@ -3,6 +3,11 @@
   programs.taskwarrior = {
     enable = true;
     package = pkgs.taskwarrior3;
+
+    config = {
+      uda.priority.values = "H,M,,L";
+      urgency.uda.priority.L.coefficient = -0.1;
+    };
   };
 
   my.persistence.directories = [
