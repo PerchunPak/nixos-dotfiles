a15fec054974f68a4a1d97f4fe4eb5923b1464ca
PerchunPak <git@perchun.it>
2025-05-10 15:14:31 +0200

    Add `aws-work` utility

--- modules/home/aws/default.nix
+++ modules/home/aws/default.nix
@@ -1,13 +1,34 @@
-{ lib, config, ... }:
 {
-  age.secrets.aws.file = lib.my.toSecretFile "aws";
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+{
+  age.secrets.aws-dev.file = lib.my.toSecretFile "aws-dev";
+  age.secrets.aws-perchun.file = lib.my.toSecretFile "aws-perchun";
+  age.secrets.aws-fish.file = lib.my.toSecretFile "aws-fish";
 
   programs.awscli = {
     enable = true;
     credentials = {
       dev = {
-        "credential_process" = "bash -c 'cat ${config.age.secrets.aws.path}'";
+        "credential_process" = "bash -c 'cat ${config.age.secrets.aws-dev.path}'";
+      };
+      perchun = {
+        "credential_process" = "bash -c 'cat ${config.age.secrets.aws-perchun.path}'";
       };
     };
+    settings = {
+      "profile perchun".region = "eu-central-1";
+    };
   };
+
+  home.packages = with pkgs; [
+    (writeShellScriptBin "aws-work" ''
+      fish ${config.age.secrets.aws-fish.path}
+    '')
+
+    ssm-session-manager-plugin
+  ];
 }
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,7 +2,9 @@ let
   perchun = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICBAYRpKyd+XVaQB766vBWxZHEB34RoLpFFWJKcxo4Ze";
 in
 {
-  "aws.age".publicKeys = [ perchun ];
+  "aws-dev.age".publicKeys = [ perchun ];
+  "aws-perchun.age".publicKeys = [ perchun ];
+  "aws-fish.age".publicKeys = [ perchun ];
   "gpg-key.age".publicKeys = [ perchun ];
   "ssh-config.age".publicKeys = [ perchun ];
 }
