cc04509f7a86906d717bfe3be166abb48a4679e9
PerchunPak <git@perchun.it>
2025-05-11 13:45:37 +0200

    Remove variables in hyprland settings
    They were included just as an example from the example config.

--- modules/home/hyprland/settings.nix
+++ modules/home/hyprland/settings.nix
@@ -20,68 +20,63 @@ in
       "[workspace special:btop silent] uwsm app -- kitty btop"
     ] ++ (lib.lists.optional (!nixosConfig.my.economInternetTraffic) "uwsm app -s b -- variety");
 
-    "$mainMod" = "SUPER";
-    "$terminal" = "kitty";
-    "$fileManager" = "nautilus";
-    "$menu" = "rofi -show drun";
-
     bind = [
       # Example binds, see https://wiki.hyprland.org/Configuring/Binds/ for more
-      "$mainMod, Q, exec, $terminal"
-      "$mainMod, C, killactive,"
-      "$mainMod, M, exec, hyprctl dispatch dpms on"
-      "$mainMod, E, exec, $fileManager"
-      "$mainMod, V, togglefloating,"
-      "$mainMod, R, exec, $menu"
-      "$mainMod, P, pseudo, # dwindle"
-      "$mainMod, J, togglesplit, # dwindle"
-      "$mainMod, F, fullscreen"
-      "$mainMod, L, exec, hyprlock"
-      "$mainMod, K, exec, ${wlogout-script}"
-      "$mainMod, G, exec, hyprpanel toggleWindow settings-dialog"
-      "$mainMod, O, exec, bash -c 'pgrep vesktop -f | xargs kill'"
-      "$mainMod, W, exec, cliphistory"
-      "$mainMod, B, exec, variety --next"
-      "$mainMod SHIFT, B, exec, variety --previous"
+      "SUPER, Q, exec, kitty"
+      "SUPER, C, killactive,"
+      "SUPER, M, exec, hyprctl dispatch dpms on"
+      "SUPER, E, exec, nautilus"
+      "SUPER, V, togglefloating,"
+      "SUPER, R, exec, rofi -show drun"
+      "SUPER, P, pseudo, # dwindle"
+      "SUPER, J, togglesplit, # dwindle"
+      "SUPER, F, fullscreen"
+      "SUPER, L, exec, hyprlock"
+      "SUPER, K, exec, ${wlogout-script}"
+      "SUPER, G, exec, hyprpanel toggleWindow settings-dialog"
+      "SUPER, O, exec, bash -c 'pgrep vesktop -f | xargs kill'"
+      "SUPER, W, exec, cliphistory"
+      "SUPER, B, exec, variety --next"
+      "SUPER SHIFT, B, exec, variety --previous"
 
       # Move focus with mainMod + arrow keys
-      "$mainMod, left, movefocus, l"
-      "$mainMod, right, movefocus, r"
-      "$mainMod, up, movefocus, u"
-      "$mainMod, down, movefocus, d"
+      "SUPER, left, movefocus, l"
+      "SUPER, right, movefocus, r"
+      "SUPER, up, movefocus, u"
+      "SUPER, down, movefocus, d"
 
       # Switch workspaces with mainMod + [0-9]
-      "$mainMod, 1, workspace, 1"
-      "$mainMod, 2, workspace, 2"
-      "$mainMod, 3, workspace, 3"
-      "$mainMod, 4, workspace, 4"
-      "$mainMod, 5, workspace, 5"
-      "$mainMod, 6, workspace, 6"
-      "$mainMod, 7, workspace, 7"
-      "$mainMod, 8, workspace, 8"
-      "$mainMod, 9, workspace, 9"
-      "$mainMod, 0, workspace, 10"
+      "SUPER, 1, workspace, 1"
+      "SUPER, 2, workspace, 2"
+      "SUPER, 3, workspace, 3"
+      "SUPER, 4, workspace, 4"
+      "SUPER, 5, workspace, 5"
+      "SUPER, 6, workspace, 6"
+      "SUPER, 7, workspace, 7"
+      "SUPER, 8, workspace, 8"
+      "SUPER, 9, workspace, 9"
+      "SUPER, 0, workspace, 10"
 
       # Move active window to a workspace with mainMod + SHIFT + [0-9]
-      "$mainMod SHIFT, 1, movetoworkspace, 1"
-      "$mainMod SHIFT, 2, movetoworkspace, 2"
-      "$mainMod SHIFT, 3, movetoworkspace, 3"
-      "$mainMod SHIFT, 4, movetoworkspace, 4"
-      "$mainMod SHIFT, 5, movetoworkspace, 5"
-      "$mainMod SHIFT, 6, movetoworkspace, 6"
-      "$mainMod SHIFT, 7, movetoworkspace, 7"
-      "$mainMod SHIFT, 8, movetoworkspace, 8"
-      "$mainMod SHIFT, 9, movetoworkspace, 9"
-      "$mainMod SHIFT, 0, movetoworkspace, 10"
+      "SUPER SHIFT, 1, movetoworkspace, 1"
+      "SUPER SHIFT, 2, movetoworkspace, 2"
+      "SUPER SHIFT, 3, movetoworkspace, 3"
+      "SUPER SHIFT, 4, movetoworkspace, 4"
+      "SUPER SHIFT, 5, movetoworkspace, 5"
+      "SUPER SHIFT, 6, movetoworkspace, 6"
+      "SUPER SHIFT, 7, movetoworkspace, 7"
+      "SUPER SHIFT, 8, movetoworkspace, 8"
+      "SUPER SHIFT, 9, movetoworkspace, 9"
+      "SUPER SHIFT, 0, movetoworkspace, 10"
 
       # Example special workspace (scratchpad)
-      "$mainMod, S, togglespecialworkspace"
-      "$mainMod SHIFT, S, movetoworkspace, special"
-      "$mainMod, N, togglespecialworkspace, btop"
+      "SUPER, S, togglespecialworkspace"
+      "SUPER SHIFT, S, movetoworkspace, special"
+      "SUPER, N, togglespecialworkspace, btop"
 
       # Scroll through existing workspaces with mainMod + scroll
-      "$mainMod, mouse_down, workspace, e+1"
-      "$mainMod, mouse_up, workspace, e-1"
+      "SUPER, mouse_down, workspace, e+1"
+      "SUPER, mouse_up, workspace, e-1"
 
       ", PRINT, exec, hyprshot --mode region --clipboard-only --silent --freeze"
 
@@ -93,8 +88,8 @@ in
 
     # Move/resize windows with mainMod + LMB/RMB and dragging
     bindm = [
-      "$mainMod, mouse:272, movewindow"
-      "$mainMod, mouse:273, resizewindow"
+      "SUPER, mouse:272, movewindow"
+      "SUPER, mouse:273, resizewindow"
     ];
 
     general = {
