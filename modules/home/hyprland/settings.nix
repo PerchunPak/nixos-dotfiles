56797c263fb4bf14bb9ab6c3d0fb6bdeb58c6419
PerchunPak <git@perchun.it>
2025-05-11 13:46:37 +0200

    Overhaul hyprland keybinds

--- modules/home/hyprland/settings.nix
+++ modules/home/hyprland/settings.nix
@@ -21,22 +21,19 @@ in
     ] ++ (lib.lists.optional (!nixosConfig.my.economInternetTraffic) "uwsm app -s b -- variety");
 
     bind = [
-      # Example binds, see https://wiki.hyprland.org/Configuring/Binds/ for more
-      "SUPER, Q, exec, kitty"
+      "SUPER, A, exec, kitty"
       "SUPER, C, killactive,"
       "SUPER, M, exec, hyprctl dispatch dpms on"
-      "SUPER, E, exec, nautilus"
+      "SUPER, ., exec, nautilus"
       "SUPER, V, togglefloating,"
-      "SUPER, R, exec, rofi -show drun"
-      "SUPER, P, pseudo, # dwindle"
-      "SUPER, J, togglesplit, # dwindle"
+      "SUPER, D, exec, rofi -show drun"
       "SUPER, F, fullscreen"
       "SUPER, L, exec, hyprlock"
       "SUPER, K, exec, ${wlogout-script}"
-      "SUPER, G, exec, hyprpanel toggleWindow settings-dialog"
-      "SUPER, O, exec, bash -c 'pgrep vesktop -f | xargs kill'"
-      "SUPER, W, exec, cliphistory"
+      "SUPER, X, exec, cliphistory"
       "SUPER, B, exec, variety --next"
+      "SUPER, F9, exec, hyprpanel toggleWindow settings-dialog"
+      "SUPER, F10, exec, bash -c 'pgrep vesktop -f | xargs kill'"
       "SUPER SHIFT, B, exec, variety --previous"
 
       # Move focus with mainMod + arrow keys
@@ -45,38 +42,29 @@ in
       "SUPER, up, movefocus, u"
       "SUPER, down, movefocus, d"
 
-      # Switch workspaces with mainMod + [0-9]
       "SUPER, 1, workspace, 1"
       "SUPER, 2, workspace, 2"
       "SUPER, 3, workspace, 3"
       "SUPER, 4, workspace, 4"
-      "SUPER, 5, workspace, 5"
-      "SUPER, 6, workspace, 6"
-      "SUPER, 7, workspace, 7"
-      "SUPER, 8, workspace, 8"
-      "SUPER, 9, workspace, 9"
-      "SUPER, 0, workspace, 10"
+      "SUPER, Q, workspace, 5"
+      "SUPER, W, workspace, 6"
+      "SUPER, E, workspace, 7"
+      "SUPER, R, workspace, 8"
+      "SUPER, T, workspace, 9"
 
-      # Move active window to a workspace with mainMod + SHIFT + [0-9]
       "SUPER SHIFT, 1, movetoworkspace, 1"
       "SUPER SHIFT, 2, movetoworkspace, 2"
       "SUPER SHIFT, 3, movetoworkspace, 3"
       "SUPER SHIFT, 4, movetoworkspace, 4"
-      "SUPER SHIFT, 5, movetoworkspace, 5"
-      "SUPER SHIFT, 6, movetoworkspace, 6"
-      "SUPER SHIFT, 7, movetoworkspace, 7"
-      "SUPER SHIFT, 8, movetoworkspace, 8"
-      "SUPER SHIFT, 9, movetoworkspace, 9"
-      "SUPER SHIFT, 0, movetoworkspace, 10"
+      "SUPER SHIFT, Q, movetoworkspace, 5"
+      "SUPER SHIFT, W, movetoworkspace, 6"
+      "SUPER SHIFT, E, movetoworkspace, 7"
+      "SUPER SHIFT, R, movetoworkspace, 8"
+      "SUPER SHIFT, T, movetoworkspace, 9"
 
-      # Example special workspace (scratchpad)
       "SUPER, S, togglespecialworkspace"
       "SUPER SHIFT, S, movetoworkspace, special"
       "SUPER, N, togglespecialworkspace, btop"
-
-      # Scroll through existing workspaces with mainMod + scroll
-      "SUPER, mouse_down, workspace, e+1"
-      "SUPER, mouse_up, workspace, e-1"
 
       ", PRINT, exec, hyprshot --mode region --clipboard-only --silent --freeze"
 
