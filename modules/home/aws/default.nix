09d1bab561928043d75e21049e5f9d33477cc501
PerchunPak <git@perchun.it>
2025-06-13 21:21:39 +0200

    Fix sorting in `.aws/credentials`

--- modules/home/aws/default.nix
+++ modules/home/aws/default.nix
@@ -13,23 +13,24 @@
 
   programs.awscli = {
     enable = true;
-    credentials = {
-      dev = {
-        "credential_process" = "bash -c 'cat ${config.age.secrets.aws-dev.path}'";
-      };
-      default = {
-        "credential_process" = "bash -c 'cat ${config.age.secrets.aws-dev.path}'";
-      };
-      perchun = {
-        "credential_process" = "bash -c 'cat ${config.age.secrets.aws-perchun.path}'";
-      };
-    };
     settings = {
       "profile dev".region = "eu-central-1";
       "profile default".region = "eu-central-1";
       "profile perchun".region = "eu-central-1";
     };
   };
+
+  # home-manager module sorts sections alphabetically, which is not what I want
+  home.file.".aws/credentials".text = ''
+    [dev]
+    credential_process=bash -c 'cat ''${XDG_RUNTIME_DIR}/agenix/aws-dev'
+
+    [default]
+    credential_process=bash -c 'cat ''${XDG_RUNTIME_DIR}/agenix/aws-dev'
+
+    [perchun]
+    credential_process=bash -c 'cat ''${XDG_RUNTIME_DIR}/agenix/aws-perchun'
+  '';
 
   home.packages = with pkgs; [
     (writeShellScriptBin "aws-work" ''
