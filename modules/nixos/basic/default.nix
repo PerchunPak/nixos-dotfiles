83b0996ddb865a774089d1d9ce85c08b9b062c16
PerchunPak <git@perchun.it>
2025-06-15 23:07:27 +0200

    Add support for ntfs

--- modules/nixos/basic/default.nix
+++ modules/nixos/basic/default.nix
@@ -43,6 +43,8 @@
 
   services.upower.enable = true;
 
+  boot.supportedFilesystems = [ "ntfs" ];
+
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "23.11";
 }
