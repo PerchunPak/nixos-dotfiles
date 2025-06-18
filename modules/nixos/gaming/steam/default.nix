2374fcd19e416343ce60930fc2181474f4ba200f
PerchunPak <git@perchun.it>
2025-06-18 18:46:32 +0200

    Enable Steam's `gamemode`

--- modules/nixos/gaming/steam/default.nix
+++ modules/nixos/gaming/steam/default.nix
@@ -20,6 +20,7 @@ in
       gamescopeSession.enable = true;
       extraCompatPackages = with pkgs; [ proton-ge-bin ];
     };
+    programs.gamemode.enable = true;
 
     environment.variables = {
       FONTCONFIG_PATH = "/etc/fonts";
