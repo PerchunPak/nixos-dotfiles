d81bf58f44907bf79ce6416c34bb2262a2a06194
PerchunPak <git@perchun.it>
2025-06-23 21:44:49 +0200

    Revert "We don't have enough internet in this month"
    This reverts commit 27e0d056aefa80d2b06986c154c7deb73b30c8ea.

--- systems/x86_64-linux/perchun-laptop/default.nix
+++ systems/x86_64-linux/perchun-laptop/default.nix
@@ -12,7 +12,6 @@
     user.password = "$y$j9T$1YPlnzEYbO2DKNH315Rx80$0Pz6NUYqmHGYMJ9KB0pEJ9Ph3hvWrRP8XV3HOap0q/1";
     nvidia.enable = true;
     hyprland.monitors = [ "eDP-1,1920x1080@144,0x0,1" ];
-    economInternetTraffic = true;
   };
 
   services.power-profiles-daemon.enable = true;
