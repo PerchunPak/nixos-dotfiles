e96f8d9ccf17ecea356e3d955ce540adf6ae33ef
PerchunPak <git@perchun.it>
2025-07-04 19:08:04 +0200

    nvim: rebind flash escape back to ctrl f
    When you press / and then Esc, it triggers flash.nvim

--- nvim/lua/my/plugins/flash.lua
+++ nvim/lua/my/plugins/flash.lua
@@ -7,7 +7,7 @@ return {
     { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
     { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
     { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    { "<Esc>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+    { "<c-f>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
   },
   after = function()
     require('flash').setup {
