4d2946ebb26a4fa8248126f62186797f45cd5286
PerchunPak <git@perchun.it>
2025-08-13 14:48:47 +0200

    Fix my cachix instance

--- modules/nixos/cachix/default.nix
+++ modules/nixos/cachix/default.nix
@@ -11,7 +11,7 @@
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
-      "perchunpak.cachix.org:XrmYympgIrmfNx2e0YhMAEzkX0IYgUfh2ou3Ev7a7eE="
+      "perchunpak.cachix.org-1:iTR6t8bKHw9ut4Fr4/8A0bjM/X67hsdR0I3F/PCUY9w="
     ];
   };
 }
