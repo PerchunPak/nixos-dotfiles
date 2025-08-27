e6da313990c78cd12be85501f72343f289c2b13a
PerchunPak <git@perchun.it>
2025-08-27 11:34:47 +0200

    Update stable nixpkgs

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
 
     # my icons and other binary files I need
     storage = {
