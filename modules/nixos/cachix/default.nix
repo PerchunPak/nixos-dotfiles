abb58e583a0beab5e47e68aac6e34cfe36973380
PerchunPak <git@perchun.it>
2025-09-04 16:18:37 +0200

    Remove lix cache
    I don't think I ever saw it working

--- modules/nixos/cachix/default.nix
+++ modules/nixos/cachix/default.nix
@@ -4,13 +4,11 @@
     substituters = lib.mkForce [
       "https://nixos-cache-proxy.cofob.dev"
       "https://nix-community.cachix.org"
-      "https://cache.lix.systems"
       "https://perchunpak.cachix.org"
     ];
 
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
       "perchunpak.cachix.org-1:iTR6t8bKHw9ut4Fr4/8A0bjM/X67hsdR0I3F/PCUY9w="
     ];
   };
