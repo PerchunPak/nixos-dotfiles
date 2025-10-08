ee4d432043c498df8bca095f69483097c9303383
PerchunPak <git@perchun.it>
2025-10-08 15:32:07 +0200

    Add second monitor

--- systems/x86_64-linux/perchun-laptop/default.nix
+++ systems/x86_64-linux/perchun-laptop/default.nix
@@ -11,7 +11,10 @@
     vm.enable = true;
     user.password = "$y$j9T$1YPlnzEYbO2DKNH315Rx80$0Pz6NUYqmHGYMJ9KB0pEJ9Ph3hvWrRP8XV3HOap0q/1";
     nvidia.enable = true;
-    hyprland.monitors = [ "eDP-1,1920x1080@144,0x0,1" ];
+    hyprland.monitors = [
+      "eDP-1,1920x1080@144,0x0,1"
+      "HDMI-A-1,1920x1080@60,1920x0,1"
+    ];
     ydotool.enable = true;
   };
 
