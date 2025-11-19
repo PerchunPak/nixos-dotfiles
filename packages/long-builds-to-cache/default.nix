cb98888cbe51fdeeab1e79dd763c214b7b9fe5a4
PerchunPak <git@perchun.it>
2025-11-19 10:51:25 +0100

    Add meld and OBS to long builds list

--- packages/long-builds-to-cache/default.nix
+++ packages/long-builds-to-cache/default.nix
@@ -5,7 +5,11 @@
 pkgs.buildEnv {
   name = "long-builds-to-cache";
   paths =
-    (with pkgs; [ lix ])
+    (with pkgs; [
+      lix
+      obs-studio
+      meld
+    ])
     ++ (with inputs.catppuccin.packages.${pkgs.system}; [
       whiskers
       cursors
