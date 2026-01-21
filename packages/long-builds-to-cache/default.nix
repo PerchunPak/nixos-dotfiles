61de4bd3886cce3d7022d180a8e12451ad315025
PerchunPak <git@perchun.it>
2026-01-21 16:26:37 +0100

    Remove `with pkgs` in `long-builds-to-cache`

--- packages/long-builds-to-cache/default.nix
+++ packages/long-builds-to-cache/default.nix
@@ -1,15 +1,18 @@
 {
+  buildEnv,
   inputs,
-  pkgs,
+  system,
+  lix,
 }:
-pkgs.buildEnv {
+buildEnv {
   name = "long-builds-to-cache";
-  paths =
-    (with pkgs; [ lix ])
-    ++ (with inputs.catppuccin.packages.${pkgs.system}; [
-      whiskers
-      cursors
-    ])
-    ++ (with inputs.nix-index-database.packages.x86_64-linux; [ comma-with-db ]);
+  paths = [
+    lix
+  ]
+  ++ (with inputs.catppuccin.packages.${system}; [
+    whiskers
+    cursors
+  ])
+  ++ (with inputs.nix-index-database.packages.${system}; [ comma-with-db ]);
   ignoreCollisions = true;
 }
