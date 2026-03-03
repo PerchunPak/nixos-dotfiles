082d1da4f83d67ede703ffc1d8efbc776d3c96be
PerchunPak <git@perchun.it>
2026-03-03 17:27:27 +0100

    Patch nixpkgs through katrinafyi/nix-patcher

---
+++ modules/home/zellij/plugins-module.nix
@@ -0,0 +1,46 @@
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}:
+let
+  cfg = config.programs.zellij;
+in
+{
+  options = {
+    programs.zellij = {
+      plugins = lib.mkOption {
+        type = lib.types.listOf lib.types.package;
+        default = [ ];
+        example = lib.literalExpression ''
+          with pkgs.zellijPlugins; [ ghost harpoon nvim-nav-plugin ]
+        '';
+        description = "List of Zellij plugins";
+      };
+    };
+  };
+
+  config = {
+    programs.zellij = {
+      package = pkgs.zellij.override {
+        extraPackages = lib.concatLists (lib.map (x: x.runtimeDeps or [ ]) cfg.plugins);
+      };
+
+      settings = {
+        plugins = lib.mkIf (cfg.plugins != [ ]) (
+          lib.mergeAttrsList (
+            lib.map (plugin: {
+              ${plugin.pname}._props = {
+                location = "file:${plugin}";
+              };
+            }) cfg.plugins
+          )
+        );
+        load_plugins = lib.mkIf (cfg.plugins != [ ]) {
+          _children = lib.map (x: { ${x.pname} = [ ]; }) cfg.plugins;
+        };
+      };
+    };
+  };
+}
