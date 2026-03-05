afc2d88f66cdf61e8568578a848adbc88c26a133
PerchunPak <git@perchun.it>
2026-03-05 13:57:07 +0100

    zellij: remove autolock plugin

--- modules/home/zellij/default.nix
+++ modules/home/zellij/default.nix
@@ -12,17 +12,8 @@
       show_startup_tips = false;
       scroll_buffer_size = 1000000;
       scrollback_lines_to_serialize = 0;
-
-      plugins = {
-        autolock = {
-          triggers = "v|nvim|vim|lazygit";
-          reaction_seconds = "1.0";
-        };
-      };
     };
     extraConfig = builtins.readFile ./config.kdl;
-
-    plugins = [ pkgs.zellijPlugins.autolock ];
   };
 
   my.persistence.directories = [ ".cache/zellij" ];
