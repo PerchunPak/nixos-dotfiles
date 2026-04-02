e284131f9a373b4feae69e5c60eb93ab29b6f194
Perchun Pak <git@perchun.it>
2026-04-03 00:41:21 +0200

    Uninstall AWS

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,10 +2,6 @@ let
   perchun = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICBAYRpKyd+XVaQB766vBWxZHEB34RoLpFFWJKcxo4Ze";
 in
 {
-  "aws-dev.age".publicKeys = [ perchun ];
-  "aws-perchun.age".publicKeys = [ perchun ];
-  "aws-work-start.age".publicKeys = [ perchun ];
-  "aws-work-stop.age".publicKeys = [ perchun ];
   "gpg-key.age".publicKeys = [ perchun ];
   "ssh-config.age".publicKeys = [ perchun ];
 }
