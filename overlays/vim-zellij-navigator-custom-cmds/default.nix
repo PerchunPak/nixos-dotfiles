9a7442c82a704c54432892ad950fe69d265a1c51
Perchun Pak <git@perchun.it>
2026-04-23 22:07:23 +0200

    Rebase zellij plugins

--- overlays/vim-zellij-navigator-custom-cmds/default.nix
+++ overlays/vim-zellij-navigator-custom-cmds/default.nix
@@ -1,7 +1,7 @@
 { ... }:
 final: prev: {
   zellijPlugins = prev.zellijPlugins // {
-    vim-zellij-navigator = final.zellijPlugins.wrapper (
+    vim-zellij-navigator = final.zellijPlugins.wrapper "vim-zellij-navigator" (
       prev.zellijPlugins.vim-zellij-navigator.unwrapped.overrideAttrs (old: {
         patches = old.patches or [ ] ++ [
           ./custom-cmds.patch
