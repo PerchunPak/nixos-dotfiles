5a24f4210342b79ec6119583df890322d2ef1c9f
Perchun Pak <git@perchun.it>
2026-04-25 13:15:52 +0200

    Symlink zellij plugins to .config/zellij/plugins
    This avoids permission access reprompts on each plugin update

--- modules/home/zellij/plugins-module.nix
+++ modules/home/zellij/plugins-module.nix
@@ -22,29 +22,51 @@ in
   };
 
   config = {
+    # on every plugin update, zellij asks for permissions again, because
+    # the plugin path has changed (=/nix/store path has changed)
+    # to avoid that, we symlink all plugins to `.config/zellij/plugins` and
+    # use those paths
+    xdg.configFile = lib.listToAttrs (
+      lib.map (
+        plugin:
+        let
+          pluginName = lib.removePrefix "zellij-" plugin.pname;
+        in
+        {
+          name = "zellij/plugins/${pluginName}.wasm";
+          value.source = plugin;
+        }
+      ) cfg.plugins
+    );
+
     programs.zellij = {
       package = pkgs.zellij.override {
         extraPackages = lib.concatLists (lib.map (x: x.runtimeDeps or [ ]) cfg.plugins);
       };
 
       settings = {
+        # define plugin aliases
         plugins = lib.mkIf (cfg.plugins != [ ]) (
           lib.mergeAttrsList (
             lib.map (
               plugin:
               let
                 pluginName = lib.removePrefix "zellij-" plugin.pname;
+                pluginPath = "${config.xdg.configHome}/zellij/plugins/${pluginName}.wasm";
               in
               {
                 ${pluginName}._props = {
-                  location = "file:${plugin}";
+                  location = "file:${pluginPath}";
                 };
               }
             ) cfg.plugins
           )
         );
+        # auto-load plugins on start
         load_plugins = lib.mkIf (cfg.plugins != [ ]) {
-          _children = lib.map (x: { ${x.pname} = [ ]; }) cfg.plugins;
+          _children = lib.map (plugin: {
+            ${lib.removePrefix "zellij-" plugin.pname} = [ ];
+          }) cfg.plugins;
         };
       };
     };
