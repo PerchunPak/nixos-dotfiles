9d017a2ea65fba5636c13cb98c8765da8b41c959
Perchun Pak <git@perchun.it>
2026-04-27 23:36:45 +0200

    Update equibop to 2.2.0

--- .github/update_nixpkgs_fork.sh
+++ .github/update_nixpkgs_fork.sh
@@ -10,6 +10,9 @@ nix run ".#pkgs.$system.nixpkgs.gh-cherry-pick" -- \
   --target PerchunPak/nixpkgs@patched \
   --first-hard-reset-to "NixOS/nixpkgs/$nixpkgs_rev" \
   PerchunPak/nixpkgs@zellij-plugins \
-  PerchunPak/nixpkgs@actual
+  PerchunPak/nixpkgs@actual \
+  PerchunPak/nixpkgs@equibop-2.2.0 \
+  `: # updates cyclopts, so gh-cherry-pick builds` \
+  PerchunPak/nixpkgs/a4ba4b34b029face2600564e3b6e130eec4e91cf
 
 nix flake update nixpkgs
