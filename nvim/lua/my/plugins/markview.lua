592f5e6dc6ad97ab480115fccd10d59bbe2ac01d
Perchun Pak <git@perchun.it>
2026-05-08 10:21:15 +0200

    nvim: disable markview by default

--- nvim/lua/my/plugins/markview.lua
+++ nvim/lua/my/plugins/markview.lua
@@ -1,4 +1,23 @@
 return {
   'markview.nvim',
   lazy = false,
+  after = function()
+    require('markview').setup {
+      preview = { enable = false },
+    }
+  end,
+  keys = {
+    {
+      '<leader>m',
+      '<cmd>Markview toggle<cr>',
+      mode = 'n',
+      desc = 'Toggles `markview` previews',
+    },
+    {
+      '<leader>M',
+      '<cmd>Markview splitToggle<cr>',
+      mode = 'n',
+      desc = 'Toggles `markview` split previews',
+    },
+  },
 }
