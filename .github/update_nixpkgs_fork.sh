db552ed9089c1916b5948c06a9714444831d5102
Perchun Pak <git@perchun.it>
2026-05-17 01:31:45 +0200

    Rename gh-cherry-pick to ghcherry

--- .github/update_nixpkgs_fork.sh
+++ .github/update_nixpkgs_fork.sh
@@ -6,7 +6,7 @@ nixpkgs_rev=$(
   nix flake metadata --json \
   | jq -er '.locks.nodes."nixpkgs-upstream".locked.rev'
 )
-nix run ".#pkgs.$system.nixpkgs.gh-cherry-pick" -- \
+nix run ".#pkgs.$system.nixpkgs.ghcherry" -- \
   --target PerchunPak/nixpkgs@patched \
   --first-hard-reset-to "NixOS/nixpkgs/$nixpkgs_rev" \
   `: # zellijPlugins: init` \
