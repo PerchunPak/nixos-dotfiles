9303c1d5044713e04f4bd7338ac9ccf33bb8f4ba
Perchun Pak <git@perchun.it>
2026-05-18 17:00:12 +0200

    Enable QT theming

---
+++ modules/home/qt/default.nix
@@ -0,0 +1,15 @@
+{
+  qt = {
+    enable = true;
+    platformTheme.name = "qtct";
+    style.name = "kvantum";
+    kvantum = {
+      enable = true;
+      settings = {
+        General = {
+          theme = "catppuccin-mocha-maroon";
+        };
+      };
+    };
+  };
+}
