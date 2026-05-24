d396159d894aad9f9eed2717eadf916ba66e09ed
Perchun Pak <git@perchun.it>
2026-05-24 20:41:37 +0200

    Don't add merged PRs to my nixpkgs fork

--- .github/update_nixpkgs_fork.sh
+++ .github/update_nixpkgs_fork.sh
@@ -12,12 +12,6 @@ nix run ".#pkgs.$system.nixpkgs.ghcherry" -- \
   `: # zellijPlugins: init` \
   https://github.com/NixOS/nixpkgs/pull/511825 \
   `: # actual-client: init` \
-  https://github.com/NixOS/nixpkgs/pull/506686 \
-  `: # equibop: 2.1.9 -> 2.2.0` \
-  https://github.com/NixOS/nixpkgs/pull/514209 \
-  `: # updates cyclopts, so it builds` \
-  https://github.com/NixOS/nixpkgs/pull/514199 \
-  `: # wayle: 0.2.3 -> 0.3.0` \
-  https://github.com/NixOS/nixpkgs/pull/521497
+  https://github.com/NixOS/nixpkgs/pull/506686
 
 nix flake update nixpkgs
