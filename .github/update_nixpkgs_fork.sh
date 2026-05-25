95e348c971c6ecd2c72e06926e55ddfd1d5cf2b3
Perchun Pak <git@perchun.it>
2026-05-25 22:27:20 +0200

    Update kulala and wayle

--- .github/update_nixpkgs_fork.sh
+++ .github/update_nixpkgs_fork.sh
@@ -12,6 +12,12 @@ nix run ".#pkgs.$system.nixpkgs.ghcherry" -- \
   `: # zellijPlugins: init` \
   https://github.com/NixOS/nixpkgs/pull/511825 \
   `: # actual-client: init` \
-  https://github.com/NixOS/nixpkgs/pull/506686
+  https://github.com/NixOS/nixpkgs/pull/506686 \
+  `: # kulala-core: init at 0.6.0; vimPlugins.kulala-nvim: 5.3.4 -> 6.1.0` \
+  https://github.com/NixOS/nixpkgs/pull/523733 \
+  `: # kulala-core: 0.6.0 -> 0.7.0; vimPlugins.kulala-nvim: 6.1.0 -> 6.2.0` \
+  https://github.com/NixOS/nixpkgs/pull/524008 \
+  `: # wayle: 0.3.0 -> 0.4.0` \
+  https://github.com/NixOS/nixpkgs/pull/524011
 
 nix flake update nixpkgs
