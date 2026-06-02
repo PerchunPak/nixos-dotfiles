bd367d50bf11f60c1e1a97d5285e2ed5d53cd513
Perchun Pak <git@perchun.it>
2026-06-02 09:22:57 +0200

    Drop merged PRs

--- .github/update_nixpkgs_fork.sh
+++ .github/update_nixpkgs_fork.sh
@@ -9,15 +9,9 @@ nixpkgs_rev=$(
 nix run ".#pkgs.$system.nixpkgs.ghcherry" -- \
   --target PerchunPak/nixpkgs@patched \
   --first-hard-reset-to "NixOS/nixpkgs/$nixpkgs_rev" \
-  `: # zellijPlugins: init` \
-  https://github.com/NixOS/nixpkgs/pull/511825 \
   `: # actual-client: init` \
   https://github.com/NixOS/nixpkgs/pull/506686 \
-  `: # kulala-core: init at 0.6.0; vimPlugins.kulala-nvim: 5.3.4 -> 6.1.0` \
-  https://github.com/NixOS/nixpkgs/pull/523733 \
-  `: # kulala-core: 0.6.0 -> 0.7.0; vimPlugins.kulala-nvim: 6.1.0 -> 6.2.0` \
-  https://github.com/NixOS/nixpkgs/pull/524008 \
-  `: # wayle: 0.3.0 -> 0.4.1` \
-  https://github.com/NixOS/nixpkgs/pull/524485
+  `: # wayle: 0.4.1 -> 0.6.0` \
+  https://github.com/NixOS/nixpkgs/pull/526419
 
 nix flake update nixpkgs
