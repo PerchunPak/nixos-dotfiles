28206a1e742816e6c13d6c394afe27ae24e84e00
Perchun Pak <git@perchun.it>
2026-06-15 20:25:10 +0200

    Add my work machine

---
+++ homes/x86_64-linux/perchun/default.nix
@@ -0,0 +1,23 @@
+{ ... }:
+{
+  home = {
+    username = "perchun";
+    homeDirectory = "/home/perchun";
+  };
+
+  my = {
+    gui.enable = false;
+  };
+
+  # `allowUnfree` for experimental nix commands
+  xdg.enable = true;
+  xdg.configFile."nixpkgs/config.nix".text = "{ allowUnfree = true; }";
+
+  programs.home-manager.enable = true;
+
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  home.stateVersion = "23.11";
+}
