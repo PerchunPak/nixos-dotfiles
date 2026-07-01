1b9201d9dda4194e3086c5fec7d4dfab6ad8ceca
Perchun Pak <git@perchun.it>
2026-07-02 00:28:19 +0200

    Fix brightness up/down keybinds

--- modules/home/hyprland/conf.lua
+++ modules/home/hyprland/conf.lua
@@ -117,8 +117,8 @@ hl.bind(
 	hl.dsp.exec_cmd("wpctl set-mute " .. default_audio .. "_SOURCE@ toggle"),
 	{ locked = true, repeating = true }
 )
-hl.bind("XF86MonBrightnessUp", hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%+"), { locked = true, repeating = true })
-hl.bind("XF86MonBrightnessDown", hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%-"), { locked = true, repeating = true })
+hl.bind("XF86MonBrightnessUp", hl.dsp.exec_cmd("brightnessctl set 5%+"), { locked = true, repeating = true })
+hl.bind("XF86MonBrightnessDown", hl.dsp.exec_cmd("brightnessctl set 5%-"), { locked = true, repeating = true })
 
 -----------------------
 ---- LOOK AND FEEL ----
