9f1ef4483dbf99286d7997424e3050ddab2bcabc
Perchun Pak <git@perchun.it>
2026-07-02 00:28:08 +0200

    hypridle: fix display not turning off

--- modules/home/hypridle/default.nix
+++ modules/home/hypridle/default.nix
@@ -7,19 +7,19 @@ in
     general = {
       lock_cmd = "pidof hyprlock || hyprlock"; # avoid starting multiple hyprlock instances.
       before_sleep_cmd = "loginctl lock-session"; # lock before suspend.
-      after_sleep_cmd = "hyprctl dispatch dpms on"; # to avoid having to press a key twice to turn on the display.
+      after_sleep_cmd = "hyprctl dispatch 'hl.dsp.dpms({ action = \"enable\" })'"; # to avoid having to press a key twice to turn on the display.
     };
 
     listener = [
       {
         timeout = 5 * 60;
-        on-timeout = "${light} -s && ${light} set 10%";
+        on-timeout = "${light} -s && ${light} set 0%";
         on-resume = "${light} -r";
       }
       {
         timeout = 8 * 60;
-        on-timeout = "hyprctl dispatch dpms off";
-        on-resume = "hyprctl dispatch dpms on";
+        on-timeout = "hyprctl dispatch 'hl.dsp.dpms({ action = \"disable\" })'";
+        on-resume = "hyprctl dispatch 'hl.dsp.dpms({ action = \"enable\" })'";
       }
       {
         timeout = 10 * 60;
