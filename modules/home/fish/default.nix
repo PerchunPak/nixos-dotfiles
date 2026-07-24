c0ab512dd19a28c12c7dc27109bf8ee6f660a059
Perchun Pak <git@perchun.it>
2026-07-24 16:50:21 +0200

    Fix fish-done notifications

--- modules/home/fish/default.nix
+++ modules/home/fish/default.nix
@@ -12,7 +12,7 @@
         ${builtins.readFile ./venv.fish}
 
         set -U __done_exclude '^(v|nvim|testNvim)'
-        set -U __done_notification_duration -1
+        set -U __done_notification_duration -2
       '';
 
     shellAliases = {
