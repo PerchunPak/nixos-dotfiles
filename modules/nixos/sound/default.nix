55a419f8d8001eeb04a08929db7af891b29981da
Perchun Pak <git@perchun.it>
2026-08-10 20:25:46 +0200

    Enable wireplumber

--- modules/nixos/sound/default.nix
+++ modules/nixos/sound/default.nix
@@ -7,6 +7,7 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    wireplumber.enable = true;
     # If you want to use JACK applications, uncomment this
     # jack.enable = true;
   };
