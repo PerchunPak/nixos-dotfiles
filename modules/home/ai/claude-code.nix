b2fbcd262e03bc3fc79426ac1bcdd0cd562788f0
Perchun Pak <git@perchun.it>
2026-09-04 21:17:47 +0200

    Support launching both Claude and GPT in Claude Code

--- modules/home/ai/claude-code.nix
+++ modules/home/ai/claude-code.nix
@@ -9,14 +9,6 @@
   config = {
     package = pkgs.claude-code;
     envDefault = {
-      ANTHROPIC_BASE_URL = "http://127.0.0.1:8317";
-      ANTHROPIC_AUTH_TOKEN = "123";
-      ANTHROPIC_DEFAULT_OPUS_MODEL = "gpt-5.6-sol";
-      ANTHROPIC_DEFAULT_SONNET_MODEL = "gpt-5.6-terra";
-      ANTHROPIC_DEFAULT_HAIKU_MODEL = "gpt-5.6-luna";
-      CLAUDE_CODE_MAX_CONTEXT_TOKENS = "372000";
-      CLAUDE_CODE_AUTO_COMPACT_WINDOW = "372000";
-
       CLAUDE_CODE_DISABLE_NONESSENTIAL_TRAFFIC = "1";
       DISABLE_NON_ESSENTIAL_MODEL_CALLS = "1";
       DISABLE_TELEMETRY = "1";
@@ -24,6 +16,21 @@
     };
     flags = {
       "--dangerously-skip-permissions" = true;
+    };
+    wrapperVariants = {
+      # chatgpt inside claude code
+      gclaude = {
+        exePath = "bin/claude";
+        envDefault = {
+          ANTHROPIC_BASE_URL = "http://127.0.0.1:8317";
+          ANTHROPIC_AUTH_TOKEN = "123";
+          ANTHROPIC_DEFAULT_OPUS_MODEL = "gpt-5.6-sol";
+          ANTHROPIC_DEFAULT_SONNET_MODEL = "gpt-5.6-terra";
+          ANTHROPIC_DEFAULT_HAIKU_MODEL = "gpt-5.6-luna";
+          CLAUDE_CODE_MAX_CONTEXT_TOKENS = "372000";
+          CLAUDE_CODE_AUTO_COMPACT_WINDOW = "372000";
+        };
+      };
     };
   };
 }
