cfc699036b14e46f41049fe4965890036a59602a
Perchun Pak <git@perchun.it>
2026-09-05 11:08:12 +0200

    Fix eval

--- modules/nixos/workarounds/from-template/default.nix
+++ modules/nixos/workarounds/from-template/default.nix
@@ -22,7 +22,10 @@
 
   nix.settings = {
     # Enable flakes and new 'nix' command
-    experimental-features = "nix-command flakes";
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
     # Deduplicate and optimize nix store
     auto-optimise-store = true;
   };
