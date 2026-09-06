760a87ffa97d23ce8e1453f29d720833b99c87c4
Perchun Pak <git@perchun.it>
2026-09-06 10:45:36 +0200

    Fix DNS

--- systems/x86_64-linux/perchun-laptop/dns.nix
+++ systems/x86_64-linux/perchun-laptop/dns.nix
@@ -3,17 +3,25 @@ let
     # OpenDNS
     "208.67.222.222"
     "208.67.220.220"
+    "2620:119:35::35"
+    "2620:119:53::53"
     # NextDNS
     "45.90.28.142"
     "45.90.30.142"
+    "2a07:a8c0::63:e785"
+    "2a07:a8c1::63:e785"
   ];
 in
 {
   services.dnsmasq = {
     enable = true;
-    settings.server = nameservers;
+    settings = {
+      server = nameservers;
+      dhcp-leasefile = false;
+    };
   };
 
-  networking.networkmanager.dns = "dnsmasq";
-  networking.nameservers = nameservers;
+  networking.networkmanager.dns = "none";
+  networking.useDHCP = false;
+  networking.dhcpcd.enable = false;
 }
