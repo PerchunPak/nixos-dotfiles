f4160f5cccfccc035415e383a4136906cf3321fe
Perchun Pak <git@perchun.it>
2026-09-21 20:06:50 +0200

    Uncomment `nixpkgs-upstream` flake input

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    # nixpkgs-upstream.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-upstream.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.11";
 
     # my icons and other binary files I need
