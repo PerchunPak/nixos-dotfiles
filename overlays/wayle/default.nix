9634c2c95abe96871bc314fb18f050043a3b7f99
Perchun Pak <git@perchun.it>
2026-09-22 18:32:32 +0200

    Don't apply patches in wayle

--- overlays/wayle/default.nix
+++ overlays/wayle/default.nix
@@ -3,19 +3,6 @@ final: prev: {
   wayle = prev.wayle.overrideAttrs (old: rec {
     src = inputs.wayle;
 
-    patches = [
-      # https://github.com/wayle-rs/wayle/pull/117
-      # fix: avoid using generic glob patterns for most application icons
-      (final.fetchpatch2 {
-        url = "https://github.com/wayle-rs/wayle/commit/2cdcbab0b3637653c5ec2c5c4d9ff46ac90e9eef.diff?full_index=1";
-        hash = "sha256-gDFpC23P3rl9TG5j+lqOE3pnJoyKZ5pD0QtHOZ5rUGk=";
-      })
-      (final.fetchpatch2 {
-        url = "https://github.com/wayle-rs/wayle/commit/3bab4b752f5b93e89333d19b85cbda55d7be1f6f.diff?full_index=1";
-        hash = "sha256-XSqZmrqEg6Db1+PZUCDxxTLu4NSF8w0sZPtAkaZT2iI=";
-      })
-    ];
-
     cargoDeps = final.rustPlatform.fetchCargoVendor {
       inherit src;
       hash = "sha256-vBE8ATQEwJIOaR53koDmbaN7H9MULZrPYwi+/L6XbpQ=";
